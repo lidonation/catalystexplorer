96798dc5e30fca27b4763912a04ba6e805defac4
etiti <emmanueltiti370@gmail.com>
2024-11-08 21:24:54 +0300

    update tsconfig

--- application/app/DataTransferObjects/UserData.php
+++ application/app/DataTransferObjects/UserData.php
@@ -8,13 +8,15 @@ use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 #[TypeScript]
 class UserData extends Data
 {
-    public function __construct(
-      public int $id,
+  public function __construct(
+    public int $id,
 
-      public string $name,
-      
-      public string $email,
+    public string $name,
 
-      public ?string $profile_photo_url
-    ) {}
+    public string $email,
+
+    public ?string $profile_photo_url,
+
+    public ?string $email_verified_at
+  ) {}
 }
