d3b803497b1408c61d5e9ab6b81df1f66270462f
Darlington Kofa <darlington@lidonation.com>
2024-11-09 16:59:56 +0300

    added custom DateFormatCast for formatting date fields on read.

--- application/app/Models/Proposal.php
+++ application/app/Models/Proposal.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use App\Models\Model;
+use App\Casts\DateFormatCast;
 use App\Enums\CatalystCurrencies;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -12,7 +12,12 @@ class Proposal extends Model
 {
     use HasFactory;
 
-    protected $casts = [
-        'currency' => CatalystCurrencies::class,
-    ];
+    protected function casts(): array
+    {
+        return [
+            'created_at' => DateFormatCast::class,
+            'updated_at' => DateFormatCast::class,
+            'currency' => CatalystCurrencies::class
+        ];
+    }
 }
