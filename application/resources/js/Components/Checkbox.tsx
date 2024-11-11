274741cd7bdcc49248e1a36d2830266b05e90cbd
theeurbanlegend <mbuto152@gmail.com>
2024-11-11 20:13:32 +0300

    LN-1255 added search component

--- application/resources/js/Components/Checkbox.tsx
+++ application/resources/js/Components/Checkbox.tsx
@@ -5,13 +5,6 @@ export default function Checkbox({
     ...props
 }: InputHTMLAttributes<HTMLInputElement>) {
     return (
-        <input
-            {...props}
-            type="checkbox"
-            className={
-                'rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800 ' +
-                className
-            }
-        />
+        <input {...props} type="checkbox" className={'rounded ' + className} />
     );
 }
