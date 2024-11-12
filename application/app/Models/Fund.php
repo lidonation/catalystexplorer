e814de3e476e7111857313271c6fcd09f712a4f8
njuma <Nahshonjumaz@gmail.com>
2024-11-12 22:49:04 +0300

    update the casts on the model

--- application/app/Models/Fund.php
+++ application/app/Models/Fund.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Casts\DateFormatCast;
+use App\Enums\CatalystCurrencies;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -16,7 +17,7 @@ class Fund extends Model
         'launched_at' => DateFormatCast::class,
         'deleted_at' => DateFormatCast::class,
         'awarded_at' => DateFormatCast::class,
-        'review_started_at' =>DateFormatCast::class,
+        'currency' => CatalystCurrencies::class
     ];
 
 }
