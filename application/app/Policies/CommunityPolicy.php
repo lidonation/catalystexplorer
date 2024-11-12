4eea96e269a9b2b78a084f94c80dc07bb247fb4c
WillardDev <willex.ox@gmail.com>
2024-11-12 09:19:51 +0300

    creating community model

---
+++ application/app/Policies/CommunityPolicy.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Community;
+use App\Enums\PermissionEnum;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CommunityPolicy extends AppPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return parent::canViewAny($user) || $user->hasAnyPermission([PermissionEnum::read_users()->value]);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Community $community): bool
+    {
+        return parent::canView($user, $community) || $user->hasAnyPermission([PermissionEnum::read_users()->value]);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return parent::canCreate($user) || $user->hasAnyPermission([PermissionEnum::create_users()->value]);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Community $community): bool
+    {
+        return parent::canUpdate($user, $community) || $user->hasAnyPermission([PermissionEnum::update_users()->value]);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Community $community): bool
+    {
+        return parent::canDelete($user, $community) || $user->hasAnyPermission([PermissionEnum::delete_users()->value]);
+    }
+}
---
+++ application/database/migrations/2024_11_11_193040_create_communities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('communities', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('content');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('status', 255);
+            $table->timestamps(0);
+            $table->timestamp('deleted_at')->nullable();
+            $table->string('slug', 255);
+        });
+
+        DB::statement('ALTER TABLE communities ADD CONSTRAINT check_status CHECK (status IN (\'draft\', \'pending\', \'accepted\', \'available\', \'claimed\', \'completed\', \'published\'))');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('communities');
+    }
+};
