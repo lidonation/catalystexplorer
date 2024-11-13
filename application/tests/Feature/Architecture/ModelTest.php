51bcfaf7c9866a555ea5ea8384ceb13be4758e1b
Darlington Kofa <darlington@lidonation.com>
2024-11-13 15:56:03 +0300

    Architecture test cleanups.

--- application/tests/Feature/Architecture/ModelTest.php
+++ application/tests/Feature/Architecture/ModelTest.php
@@ -2,12 +2,21 @@
 
 use App\Models\Model;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 arch()
     ->expect('App\Models')
     ->toBeClasses()
     ->toExtend(Model::class)
     ->ignoring(User::class);
+
+arch()
+    ->expect('App\Models')
+    ->not->toUseTrait(HasFactory::class)
+    ->ignoring([
+        User::class,
+        Model::class
+    ]);
 
 arch()
     ->expect('App\Models')
