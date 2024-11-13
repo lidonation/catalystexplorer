9f55a14b1faf197f82137e2980b36b8d1ecb308e
njuma <Nahshonjumaz@gmail.com>
2024-11-13 13:58:36 +0300

    update Fund.php

--- application/app/Models/Fund.php
+++ application/app/Models/Fund.php
@@ -4,15 +4,12 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Models\Model;
 use App\Casts\DateFormatCast;
 use App\Enums\CatalystCurrencies;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Fund extends Model
 {
-    use HasFactory;
-
     protected $casts = [
         'launched_at' => DateFormatCast::class,
         'deleted_at' => DateFormatCast::class,
