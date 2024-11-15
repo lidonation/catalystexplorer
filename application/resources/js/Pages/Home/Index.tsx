67bb6d64b7cb7c729283731486e9e7ff1c1be0f8
Darlington Kofa <darlington@lidonation.com>
2024-11-15 17:36:27 +0300

    obsessing over sticky searchbar.

--- application/resources/js/Pages/Home/Index.tsx
+++ application/resources/js/Pages/Home/Index.tsx
@@ -10,14 +10,10 @@ export default function Index({posts}: PageProps<{ posts: any; }>) {
         <>
             <Head title="Catalyst Explorer"/>
 
-            <CatalystIntro />
 
-            <div className="flex flex-col gap-8 w-full justify-center">
-                <section className='splash-wrapper py-4 sticky top-4'>
-                    <div className='container'>
-                        <SearchBar autoFocus/>
-                    </div>
-                </section>
+
+            <div className="relative flex flex-col gap-8 w-full justify-center">
+                <CatalystIntro/>
 
                 <section className="numbers-werapper py-16"></section>
 
@@ -27,7 +23,7 @@ export default function Index({posts}: PageProps<{ posts: any; }>) {
 
                 <WhenVisible fallback='loading pulse here' data="posts">
                     <section className='posts-wrapper'>
-                        <div className='container py-8 max-h-80 overflow-auto'>
+                        <div className='container py-8 max-h80 overflow-auto'>
                             <h2 className='title-2'>Your data:</h2>
                             {JSON.stringify(posts)}
                         </div>
