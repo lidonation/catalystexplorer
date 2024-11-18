1810c47c2bd5d7b8f2943abbd6bcb8a35cd5e771
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2024-11-18 16:57:43 +0300

    create the horizontal posts list
    Fixing merge conflicts

--- application/resources/js/Pages/Home/Index.tsx
+++ application/resources/js/Pages/Home/Index.tsx
@@ -1,12 +1,13 @@
 import CatalystIntro from '@/Pages/Home/Partials/CatalystIntro';
 import PostCard from '@/Pages/Posts/Partials/PostCard';
-import { PageProps } from '@/types';
+import { PageProps, Post } from '@/types';
 import { Head, WhenVisible } from '@inertiajs/react';
+import PostListLoader from '../Posts/Partials/PostListLoader';
 import ProposalCard from '../Proposals/Partials/ProposalCard';
 import ProposalCardLoading from '../Proposals/Partials/ProposalCardLoading';
 
 interface HomePageProps extends Record<string, unknown> {
-    posts: any;
+    posts: Post[];
     proposals: App.DataTransferObjects.ProposalData[];
 }
 
@@ -18,17 +19,17 @@ export default function Index({ posts, proposals }: PageProps<HomePageProps>) {
             <div className="relative flex w-full flex-col justify-center gap-8">
                 <CatalystIntro />
 
-                <section className="numbers-werapper py-16"></section>
+                <section className="numbers-wrapper py-16"></section>
 
                 <WhenVisible
-                    fallback={<ProposalCardLoading/>}
+                    fallback={<ProposalCardLoading />}
                     data="proposals"
                 >
-                    <section className="propoals-wrapper">
+                    <section className="proposals-wrapper">
                         <div className="max-h80 container overflow-auto py-8">
-                            <h2 className="title-2">Proposals:</h2>
+                            <h2 className="title-2">Proposals</h2>
                         </div>
-                        <div className="content-gap container flex flex-nowrap overflow-x-scroll">
+                        <div className="content-gap container flex flex-nowrap">
                             {proposals &&
                                 proposals.map((proposal) => (
                                     <ProposalCard
@@ -42,19 +43,22 @@ export default function Index({ posts, proposals }: PageProps<HomePageProps>) {
 
                 <section className="special-announcements-wrapper"></section>
 
-                <WhenVisible fallback="ProposalCardLoading" data="posts">
-                    <section className="posts-wrapper">
-                        <div className="max-h80 container overflow-auto py-8">
-                            <h2 className="title-2">Your data:</h2>
-                            {JSON.stringify(posts)}
-                        </div>
-                        <div className="content-gap container flex flex-nowrap overflow-x-scroll">
-                            <PostCard />
-                            <PostCard />
-                            <PostCard />
-                        </div>
-                    </section>
-                </WhenVisible>
+                <section className="posts-wrapper container flex flex-col gap-8">
+                    <div>
+                        <h2 className="title-2">Catalysts Posts</h2>
+                        <p>Latest news and posts from our community</p>
+                    </div>
+                    <WhenVisible fallback={<PostListLoader />} data="posts">
+                        <ul className="content-gap scrollable snaps-scrollable">
+                            {posts &&
+                                posts.map((post: Post) => (
+                                    <li key={post.id}>
+                                        <PostCard post={post} />
+                                    </li>
+                                ))}
+                        </ul>
+                    </WhenVisible>
+                </section>
             </div>
         </>
     );
