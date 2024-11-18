8e22e381c47712e0db58b9e4c5dbc474e01f1e68
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2024-11-18 21:45:03 +0300

    add translations for text and update types

---
+++ application/app/DataTransferObjects/PostData.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace App\DataTransferObjects;
+
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Attributes\WithCast;
+use Spatie\LaravelData\Casts\DateTimeInterfaceCast;
+use Spatie\TypeScriptTransformer\Attributes\Optional as TypeScriptOptional;
+use Spatie\TypeScriptTransformer\Attributes\TypeScript;
+
+#[TypeScript]
+final class PostData extends Data
+{
+    public function __construct(
+        public ?int $id,
+
+        public ?string $title,
+
+        public ?string $subtitle,
+
+        public ?string $summary,
+
+        public mixed $hero,
+
+        public ?string $author_gravatar,
+
+        public ?string $author_name,
+
+        public string $link,
+
+        public ?string $read_time,
+
+        public ?string $type,
+
+        #[TypeScriptOptional, WithCast(DateTimeInterfaceCast::class, format: 'Y-m-d H:i:s')]
+        public ?\DateTime $published_at,
+    ) {}
+}
