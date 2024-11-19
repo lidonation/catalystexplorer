401bf7416ab790d1332ffc278714e2e267befade
root <root@Cindy.>
2024-11-19 17:23:18 +0300

    Proposal List Component

--- application/resources/js/Pages/Proposals/Partials/ProposalList.tsx
+++ application/resources/js/Pages/Proposals/Partials/ProposalList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProposalCard from "@/Pages/Proposals/Partials/ProposalCard";
 import SecondaryButton from "@/Components/SecondaryButton";
+import { router, Link } from "@inertiajs/react";
 import ProposalData = App.DataTransferObjects.ProposalData;
 
 interface ProposalProps {
@@ -8,15 +9,18 @@ interface ProposalProps {
 }
 
 const ProposalList: React.FC<ProposalProps> = ({ proposals }) => {
+    function navigate (){
+       router.get('/proposals')
+    }
     return (
         <section className="proposals-wrapper">
             <div className="container py-8 flex justify-between items-center">
                 <div>
                     <h2 className="title-2">Proposals</h2>
-                    <p className="text-4">Proposal votes must be submitted in the official Catalyst Voting App</p>
+                    <p className="text-4 text-content-dark opacity-70">Proposal votes must be submitted in the official Catalyst Voting App</p>
                 </div>
                 <div>
-                    <SecondaryButton className="font-bold ">
+                    <SecondaryButton className="font-bold text-content-dark" onClick={navigate}>
                         See more proposals
                     </SecondaryButton>
                 </div>
