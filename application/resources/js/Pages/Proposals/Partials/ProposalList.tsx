a6edc5f41687e33e8017dc015efb2df27ab46daf
root <root@Cindy.>
2024-11-19 19:03:23 +0300

    feature/LN-1322-proposal-list-component

--- application/resources/js/Pages/Proposals/Partials/ProposalList.tsx
+++ application/resources/js/Pages/Proposals/Partials/ProposalList.tsx
@@ -23,7 +23,7 @@ const ProposalList: React.FC<ProposalProps> = ({ proposals }) => {
                 </div>
                 <div>
                     <SecondaryButton className="font-bold text-content-dark" onClick={navigate}>
-                        See more proposals
+                    {t("proposalList.buttonText")}
                     </SecondaryButton>
                 </div>
             </div>
