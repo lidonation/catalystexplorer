448809cffc3ee56e96c147c994c472e7b88cd039
Darlington Kofa <darlington@lidonation.com>
2024-11-21 20:55:27 +0300

    Resolve failing pipeline.

--- application/resources/js/utils/localizedRoute.ts
+++ application/resources/js/utils/localizedRoute.ts
@@ -4,7 +4,6 @@ import {usePage} from '@inertiajs/react';
 export function useLocalizedRoute(name: string) {
     const route = useRoute();
     const { locale } = usePage().props;
-    console.log( usePage().props);
 
     return route(`${locale||'en'}.${name}`);
 };
