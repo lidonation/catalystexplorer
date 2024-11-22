a269aaf760a72fd6c70de5664c027940c541e036
Emmanuel Titi <emmanueltiti370@gmail.com>
2024-11-22 16:04:38 +0000

    adding range picker to proposal filters

--- application/resources/js/Pages/Proposals/Index.tsx
+++ application/resources/js/Pages/Proposals/Index.tsx
@@ -1,44 +1,48 @@
-import {Head, WhenVisible} from '@inertiajs/react';
-import {useTranslation} from "react-i18next";
-import {PageProps} from "@/types";
+import ProposalCardLoading from '@/Pages/Proposals/Partials/ProposalCardLoading';
+import ProposalResults from '@/Pages/Proposals/Partials/ProposalResults';
+import { PageProps } from '@/types';
+import { Head, WhenVisible } from '@inertiajs/react';
+import { useTranslation } from 'react-i18next';
+import ProposalFilters from './Partials/ProposalFilters';
 import ProposalData = App.DataTransferObjects.ProposalData;
-import ProposalCardLoading from "@/Pages/Proposals/Partials/ProposalCardLoading";
-import ProposalResults from "@/Pages/Proposals/Partials/ProposalResults";
 
 interface HomePageProps extends Record<string, unknown> {
     proposals: ProposalData[];
 }
 
-export default function Index({proposals}: PageProps<HomePageProps>) {
-    const {t} = useTranslation();
+export default function Index({ proposals }: PageProps<HomePageProps>) {
+    const { t } = useTranslation();
     return (
         <>
             <Head title="Proposals" />
+            <Head title="Proposals" />
 
             <header>
-                <div className='container'>
-                    <h1 className="title-1">
-                        {t('proposals.proposals')}
-                    </h1>
+                <div className="container">
+                    <h1 className="title-1">{t('proposals.proposals')}</h1>
                 </div>
 
-                <div className='container'>
+                <div className="container">
                     <p className="text-content">
                         {t('proposals.pageSubtitle')}
                     </p>
                 </div>
             </header>
 
-            <div className="w-full container proposals-wrapper">
+            <section className="container flex w-full flex-col items-center justify-center">
+                <ProposalFilters />
+            </section>
+
+            <section className="proposals-wrapper container w-full">
                 <WhenVisible
-                    fallback={<ProposalCardLoading/>}
+                    fallback={<ProposalCardLoading />}
                     data="proposals"
                 >
-                    <div className='py-4'>
+                    <div className="py-4">
                         <ProposalResults proposals={proposals} />
                     </div>
                 </WhenVisible>
-            </div>
+            </section>
         </>
     );
 }
