c186e5cf96803ed54a41d5149c7146c3e1f72429
Darlington Kofa <darlington@lidonation.com>
2024-11-22 09:17:41 +0300

    wip: mix-content bug.

--- application/bootstrap/app.php
+++ application/bootstrap/app.php
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->trustProxies(at: '*');
         if (app()->environment('production')) {
-            $middleware->trustHosts(at: getenv('APP_HOSTS') ?: '*');
+            $middleware->trustHosts(at: getenv('APP_HOSTS') ?? '*');
         }
     })
     ->withExceptions(function (Exceptions $exceptions) {
