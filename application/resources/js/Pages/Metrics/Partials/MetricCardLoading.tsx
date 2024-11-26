bdbb3e2253338e58cd697d89c7d2945fa5179c9b
Willard Odongo <willard.odongo10@gmail.com>
2024-11-26 08:04:16 +0000

    Feature/ln 1261 metric card

--- application/resources/js/Pages/Metrics/Partials/MetricCardLoading.tsx
+++ application/resources/js/Pages/Metrics/Partials/MetricCardLoading.tsx
@@ -6,19 +6,19 @@ export default function MetricCardLoading() {
                     {[1, 2, 3].map((metric, index) => (
                         <div
                             key={index}
-                            className="bg-white border rounded-lg shadow-md h-48 relative"
+                            className="bg-background rounded-lg shadow-md h-48 relative"
                         >
                             <div className="animate-pulse">
-                                <div className="absolute top-4 left-10 bg-white shadow-md rounded-md px-4 py-2 z-10">
+                                <div className="absolute top-4 left-10 bg-background shadow-md rounded-md px-4 py-2 z-10">
                                     <div className="h-8 w-24 bg-slate-200 rounded mb-2"></div>
                                     <div className="h-4 w-32 bg-slate-200 rounded"></div>
                                 </div>
 
-                                <div className="absolute top-4 right-10 flex items-center">
+                                <div className="bg-background absolute top-4 right-10 flex items-center">
                                     <div className="h-6 w-20 bg-slate-200 rounded"></div>
                                 </div>
 
-                                <div className="mt-16 px-4">
+                                <div className="bg-background mt-16 px-4">
                                     <div className="h-24 bg-slate-200 rounded"></div>
                                 </div>
                             </div>
