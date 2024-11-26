cca8f1d3d170ea2939f40f7affb3719a415c6267
Bewton Maina <mbuto152@gmail.com>
2024-11-26 07:46:04 +0000

    Refactor utility imports and remove unused styleutils; add Popover,...

--- application/resources/js/Pages/Proposals/Partials/ProposalFilters.tsx
+++ application/resources/js/Pages/Proposals/Partials/ProposalFilters.tsx
@@ -1,13 +1,25 @@
 import { RangePicker } from '@/Components/RangePicker';
+import { SearchSelect } from '@/Components/SearchSelect';
 import Selector from '@/Components/Selector';
 import { useState } from 'react';
-import {ProposalSearchParams} from "../../../../types/proposal-search-params";
+import { ProposalSearchParams } from '../../../../types/proposal-search-params';
 
-export default function ProposalFilters({ filters } : { filters: ProposalSearchParams }) {
+export default function ProposalFilters({
+    filters,
+}: {
+    filters: ProposalSearchParams;
+}) {
     const [range, setRange] = useState([20, 80]);
-    const [selectedFundingStatus, setSelectedFundingStatus] = useState<string[]>(['o']);
-    const [selectedOpensourceStatus, setSelectedOpensourceStatus] = useState<string[]>([]);
-    const [selectedProjectStatus, setSelectedProjectStatus] = useState<string[]>([]);
+    const [selectedFundingStatus, setSelectedFundingStatus] = useState<
+        string[]
+    >(['o']);
+    const [selectedOpensourceStatus, setSelectedOpensourceStatus] = useState<
+        string[]
+    >([]);
+    const [selectedProjectStatus, setSelectedProjectStatus] = useState<
+        string[]
+    >([]);
+    const [selected, setSelected] = useState<string[]>([]);
 
     return (
         <div className="w-full bg-background p-4">
@@ -54,10 +66,23 @@ export default function ProposalFilters({ filters } : { filters: ProposalSearchP
                         context={'Project Status'}
                     />
                 </div>
+                <div className="flex flex-col gap-2 pb-4 pt-6">
+                    <span>Project Status</span>
+                    <SearchSelect
+                        options={[
+                            { value: 'o', label: 'Over Budget' },
+                            { value: 'n', label: 'Not Approved' },
+                            { value: 'f', label: 'Funded' },
+                            { value: 'p', label: 'Fully Paid' },
+                        ]}
+                        selected={selected}
+                        onChange={setSelected}
+                        placeholder="Select status"
+                        multiple={true}
+                    />
+                </div>
             </div>
-
             <div className="my-6 w-full border-b"></div>
-
             <div className="grid grid-cols-1 gap-x-4 gap-y-3 rounded-xl lg:grid-cols-2">
                 <div className="pb-4">
                     <RangePicker
