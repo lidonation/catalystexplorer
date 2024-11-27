72c17c2419688bf19a7cec7b96eef0aba3c5c917
etiti <emmanueltiti370@gmail.com>
2024-11-27 18:53:37 +0300

    add sort changes

--- application/resources/js/Pages/Proposals/Partials/ProposalFilters.tsx
+++ application/resources/js/Pages/Proposals/Partials/ProposalFilters.tsx
@@ -34,123 +34,147 @@ export default function ProposalFilters({
     const [selected, setSelected] = useState<string[]>([]);
 
     return (
-        <div className="w-full bg-background p-4">
-            <b>Filters Values:</b> {JSON.stringify(filters)} <br />
-            <div className="grid grid-cols-1 gap-x-4 gap-y-3 rounded-xl md:grid-cols-2 lg:grid-cols-4">
-                <div className="flex flex-col gap-2 pb-4 pt-6">
-                    <span>Funding Status</span>
-                    <Selector
-                        isMultiselect={true}
-                        options={[
-                            {
-                                value: 'o',
-                                label: t('proposals.options.overBudget'),
-                            },
-                            {
-                                value: 'n',
-                                label: t('proposals.options.notApproved'),
-                            },
-                            {
-                                value: 'f',
-                                label: t('proposals.options.funded'),
-                            },
-                            {
-                                value: 'p',
-                                label: t('proposals.options.fullyPaid'),
-                            },
-                        ]}
-                        setSelectedItems={setSelectedFundingStatus}
-                        selectedItems={selectedFundingStatus}
-                    />
-                </div>
+        <>
+            <div className="container mx-auto flex justify-end px-0 pb-4 pt-6">
+                <select
+                    value={selectedSort}
+                    onChange={handleSortChange}
+                    className="max-w-[250px] rounded-md border border-gray-300 bg-background"
+                >
+                    <option value="">{t('proposals.options.sort')}</option>
+                    <option value="created_at_asc">
+                        {t('proposals.options.oldToNew')}
+                    </option>
+                    <option value="created_at_desc">
+                        {t('proposals.options.newToOld')}
+                    </option>
+                    <option value="budget_asc">
+                        {t('proposals.options.lowToHigh')}
+                    </option>
+                    <option value="budget_desc">
+                        {t('proposals.options.highToLow')}
+                    </option>
+                </select>
+            </div>
+            <div className="w-full bg-background p-4">
+                <b>{t('proposals.options.filterValues')}:</b>{' '}
+                {JSON.stringify(filters)} <br />
+                <div className="grid grid-cols-1 gap-x-4 gap-y-3 rounded-xl md:grid-cols-2 lg:grid-cols-4">
+                    <div className="flex flex-col gap-2 pb-4 pt-6">
+                        <span>Funding Status</span>
+                        <Selector
+                            isMultiselect={true}
+                            options={[
+                                {
+                                    value: 'o',
+                                    label: t('proposals.options.overBudget'),
+                                },
+                                {
+                                    value: 'n',
+                                    label: t('proposals.options.notApproved'),
+                                },
+                                {
+                                    value: 'f',
+                                    label: t('proposals.options.funded'),
+                                },
+                                {
+                                    value: 'p',
+                                    label: t('proposals.options.fullyPaid'),
+                                },
+                            ]}
+                            setSelectedItems={setSelectedFundingStatus}
+                            selectedItems={selectedFundingStatus}
+                        />
+                    </div>
 
-                <div className="flex flex-col gap-2 pb-4 pt-6">
-                    <span>Opensource</span>
-                    <Selector
-                        isMultiselect={false}
-                        options={[
-                            {
-                                value: '1',
-                                label: t(
-                                    'proposals.options.opensourceProposals',
-                                ),
-                            },
-                            {
-                                value: '0',
-                                label: t(
-                                    'proposals.options.nonOpensourceProposals',
-                                ),
-                            },
-                        ]}
-                        setSelectedItems={setSelectedOpensourceStatus}
-                        selectedItems={selectedOpensourceStatus}
-                    />
-                </div>
+                    <div className="flex flex-col gap-2 pb-4 pt-6">
+                        <span>Opensource</span>
+                        <Selector
+                            isMultiselect={false}
+                            options={[
+                                {
+                                    value: '1',
+                                    label: t(
+                                        'proposals.options.opensourceProposals',
+                                    ),
+                                },
+                                {
+                                    value: '0',
+                                    label: t(
+                                        'proposals.options.nonOpensourceProposals',
+                                    ),
+                                },
+                            ]}
+                            setSelectedItems={setSelectedOpensourceStatus}
+                            selectedItems={selectedOpensourceStatus}
+                        />
+                    </div>
 
-                <div className="flex flex-col gap-2 pb-4 pt-6">
-                    <span>Project Status</span>
-                    <Selector
-                        isMultiselect={true}
-                        options={[
-                            {
-                                value: 'c',
-                                label: t('proposals.options.complete'),
-                            },
-                            {
-                                value: 'i',
-                                label: t('proposals.options.inProgress'),
-                            },
-                            {
-                                value: 'u',
-                                label: t('proposals.options.unfunded'),
-                            },
-                        ]}
-                        setSelectedItems={setSelectedProjectStatus}
-                        selectedItems={selectedProjectStatus}
-                    />
+                    <div className="flex flex-col gap-2 pb-4 pt-6">
+                        <span>Project Status</span>
+                        <Selector
+                            isMultiselect={true}
+                            options={[
+                                {
+                                    value: 'c',
+                                    label: t('proposals.options.complete'),
+                                },
+                                {
+                                    value: 'i',
+                                    label: t('proposals.options.inProgress'),
+                                },
+                                {
+                                    value: 'u',
+                                    label: t('proposals.options.unfunded'),
+                                },
+                            ]}
+                            setSelectedItems={setSelectedProjectStatus}
+                            selectedItems={selectedProjectStatus}
+                        />
+                    </div>
+                    <div className="flex flex-col gap-2 pb-4 pt-6">
+                        <span className="">Project Status</span>
+                        <SearchSelect
+                            options={[
+                                {
+                                    value: 'o',
+                                    label: t('proposals.options.overBudget'),
+                                },
+                                {
+                                    value: 'n',
+                                    label: t('proposals.options.notApproved'),
+                                },
+                                {
+                                    value: 'f',
+                                    label: t('proposals.options.funded'),
+                                },
+                                {
+                                    value: 'p',
+                                    label: t('proposals.options.fullyPaid'),
+                                },
+                            ]}
+                            selected={selected}
+                            onChange={setSelected}
+                            placeholder="Select"
+                            multiple={true}
+                        />
+                    </div>
                 </div>
-                <div className="flex flex-col gap-2 pb-4 pt-6">
-                    <span className="">Project Status</span>
-                    <SearchSelect
-                        options={[
-                            {
-                                value: 'o',
-                                label: t('proposals.options.overBudget'),
-                            },
-                            {
-                                value: 'n',
-                                label: t('proposals.options.notApproved'),
-                            },
-                            {
-                                value: 'f',
-                                label: t('proposals.options.funded'),
-                            },
-                            {
-                                value: 'p',
-                                label: t('proposals.options.fullyPaid'),
-                            },
-                        ]}
-                        selected={selected}
-                        onChange={setSelected}
-                        placeholder="Select"
-                        multiple={true}
-                    />
+                <div className="my-6 w-full border-b"></div>
+                <div className="grid grid-cols-1 gap-x-4 gap-y-3 rounded-xl lg:grid-cols-2">
+                    <div className="pb-4">
+                        <RangePicker
+                            context={'Budgets'}
+                            value={range}
+                            onValueChange={(newRange: []) => setRange(newRange)}
+                            max={100}
+                            min={10}
+                            step={1}
+                            defaultValue={[20, 80]}
+                        />
+                    </div>
                 </div>
             </div>
-            <div className="my-6 w-full border-b"></div>
-            <div className="grid grid-cols-1 gap-x-4 gap-y-3 rounded-xl lg:grid-cols-2">
-                <div className="pb-4">
-                    <RangePicker
-                        context={'Budgets'}
-                        value={range}
-                        onValueChange={(newRange: []) => setRange(newRange)}
-                        max={100}
-                        min={10}
-                        step={1}
-                        defaultValue={[20, 80]}
-                    />
-                </div>
-            </div>
-        </div>
+        </>
     );
 }
