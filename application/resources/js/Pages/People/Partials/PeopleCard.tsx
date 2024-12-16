b0d0ed4d6df29557087e6847c8dfd7affde66912
Cindy <cindyjoan2000@gmail.com>
2024-12-16 19:46:57 +0300

    People Card

--- application/resources/js/Pages/People/Partials/PeopleCard.tsx
+++ application/resources/js/Pages/People/Partials/PeopleCard.tsx
@@ -16,20 +16,24 @@ const PeopleCard: React.FC<PeopleProps> = ({
 }) => {
     const { t } = useTranslation();
     return (
-        <div className="w-full space-y-4 rounded-md bg-background p-4 shadow-sm">
-            <div className='w-full'>
-            <div>
-                <UserAvatar imageUrl={profilePhotoUrl} size='size-12'/>
-            </div>
-            <p className="text-2 font-bold mt-2">{name}</p>
+        <div className="w-full rounded-md bg-background p-4 shadow-sm">
+            <div className="w-full mb-2">
+                <div>
+                    <UserAvatar imageUrl={profilePhotoUrl} size="size-12" />
+                </div>
+                <p className="text-2 mt-2 font-bold">{name}</p>
             </div>
             <div className="border-t-2 border-border-secondary">
-                <div className="flex w-full justify-between pt-2 pb-4">
-                    <p className="text-4 text-content opacity-70">{t('people.ownProposals')}</p>
+                <div className="flex w-full justify-between pb-4 pt-2">
+                    <p className="text-4 text-content opacity-70">
+                        {t('people.ownProposals')}
+                    </p>
                     <p className="text-3 font-semibold">{ownProposals}</p>
                 </div>
                 <div className="flex w-full justify-between">
-                    <p className="text-4 text-content opacity-70">{t('people.coProposals')}</p>
+                    <p className="text-4 text-content opacity-70">
+                        {t('people.coProposals')}
+                    </p>
                     <p className="text-3 font-semibold">{coProposals}</p>
                 </div>
             </div>
