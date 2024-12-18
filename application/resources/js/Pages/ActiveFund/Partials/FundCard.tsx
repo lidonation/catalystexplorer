9db44dee82de0f1edccb4cc6a9402545615b730d
WillardDev <willex.ox@gmail.com>
2024-12-18 10:36:43 +0300

    added translations

--- application/resources/js/Pages/ActiveFund/Partials/FundCard.tsx
+++ application/resources/js/Pages/ActiveFund/Partials/FundCard.tsx
@@ -40,7 +40,7 @@ const FundCard: React.FC<FundCardProps> = ({ fund }) => {
           />
         ) : (
           <div className="flex items-center justify-center h-full text-gray-500">
-            No Image
+            {t('noImage')}
           </div>
         )}
       </div>
