2535e315f0a57c5ecc28bd21345d0099391b6033
Tyty.Emmanuel <emmanueltiti370@gmail.com>
2025-01-03 20:23:39 +0300

    cleanup users table

--- application/database/migrations/0001_01_01_000000_create_users_table.php
+++ application/database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->foreignId('current_team_id')->nullable()->onDelete('set null');
             $table->text('profile_photo_path')->nullable();
             $table->timestamps();
-            $table->text('two_factor_secret')->nullable();
             $table->text('two_factor_recovery_codes')->nullable();
             $table->text('short_bio')->nullable();
             $table->text('bio')->nullable();
