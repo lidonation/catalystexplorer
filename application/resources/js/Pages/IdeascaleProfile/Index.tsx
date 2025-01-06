d47dc3fb95359768cada7159325b917e479ba9eb
Cindy Joan <cindyjoan2000@gmail.com>
2025-01-06 17:23:05 +0000

    Feature/ln 1362 ce peoples page search

--- application/resources/js/Pages/IdeascaleProfile/Index.tsx
+++ application/resources/js/Pages/IdeascaleProfile/Index.tsx
@@ -1,35 +1,51 @@
+import { FiltersProvider } from '@/Context/FiltersContext';
 import { PageProps } from '@/types';
-import { Head } from '@inertiajs/react';
-import IdeascaleProfilesList from './Partials/IdeascaleProfileList';
-import IdeascaleProfilesFilters from './Partials/IdeascaleProfilesFilters';
+import { Head, WhenVisible } from '@inertiajs/react';
 import { useTranslation } from 'react-i18next';
+import { ProposalSearchParams } from '../../../types/proposal-search-params';
+import IdeascaleProfilesList from './Partials/IdeascaleProfileList';
+import IdeaScaleProfileLoader from './Partials/IdeaScaleProfileLoader';
+import IdeascaleProfilesFilters from './Partials/IdeascaleProfilesFilters';
 import IdeascaleProfilesData = App.DataTransferObjects.IdeascaleProfileData;
 
 interface IdeascaleProfilesPageProps extends Record<string, unknown> {
     ideascaleProfiles: IdeascaleProfilesData[];
+    filters: ProposalSearchParams;
 }
-const Index = ({ ideascaleProfiles }: PageProps<IdeascaleProfilesPageProps>) => {
-
+const Index = ({
+    ideascaleProfiles,
+    filters,
+}: PageProps<IdeascaleProfilesPageProps>) => {
     const { t } = useTranslation();
 
     return (
         <>
-            <Head title="Ideascale Profiles" />
+            <FiltersProvider defaultFilters={filters}>
+                <Head title="Ideascale Profiles" />
 
-            <header className="container">
-                <h1 className="title-1">{t('ideascaleProfiles.ideascaleProfiles')}</h1>
-                <p className="text-content">{t('ideascaleProfiles.pageSubtitle')}</p>
-            </header>
+                <header className="container">
+                    <h1 className="title-1">
+                        {t('ideascaleProfiles.ideascaleProfiles')}
+                    </h1>
+                    <p className="text-content">
+                        {t('ideascaleProfiles.pageSubtitle')}
+                    </p>
+                </header>
 
-            <section className="container flex w-full flex-col items-center justify-center py-8">
-                <IdeascaleProfilesFilters />
-            </section>
-
-            <div className="flex w-full flex-col items-center">
-                <section className="container py-8">
-                  <IdeascaleProfilesList ideascaleProfiles={ideascaleProfiles}/>
+                <section className="container flex w-full flex-col items-center justify-center py-8">
+                    <IdeascaleProfilesFilters />
                 </section>
-            </div>
+
+                <div className="flex w-full flex-col items-center">
+                    <section className="container py-8">
+                        <WhenVisible fallback={<IdeaScaleProfileLoader/>} data="ideascaleProfiles">
+                            <IdeascaleProfilesList
+                                ideascaleProfiles={ideascaleProfiles}
+                            />
+                        </WhenVisible>
+                    </section>
+                </div>
+            </FiltersProvider>
         </>
     );
 };
--- application/resources/js/Pages/IdeascaleProfile/Partials/IdeascaleProfilesFilters.tsx
+++ application/resources/js/Pages/IdeascaleProfile/Partials/IdeascaleProfilesFilters.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react';
-import Selector from '@/Components/Select';
-import { useTranslation } from 'react-i18next';
-import FundingStatusToggle from './FundingStatusToggle';
-import { SearchSelect } from '@/Components/SearchSelect';
 import { RangePicker } from '@/Components/RangePicker';
+import { SearchSelect } from '@/Components/SearchSelect';
+import Selector from '@/Components/Select';
+import { useFilterContext } from '@/Context/FiltersContext';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ProposalSearchParams } from '../../../../types/proposal-search-params';
+import FundingStatusToggle from './FundingStatusToggle';
+import IdeascaleProfilesSearchControls from './IdeascaleProfileSearchControls';
 
 export default function IdeascaleProfilesFilters() {
     const { t } = useTranslation();
 
-    const filters = {
+    const ideascaleFilters = {
         funds: [{ value: 'fund1', label: 'Fund 1' }],
         project_status: [],
         tags: [{ value: 'tag1', label: 'Tag 1' }],
@@ -23,63 +26,82 @@ export default function IdeascaleProfilesFilters() {
     const [projectStatus, setProjectStatus] = useState([]);
 
     return (
-        <div className="container w-full rounded-xl bg-background shadow-md p-4">
-            <div className="grid grid-cols-2 gap-4 lg:grid-cols-5 lg:gap-8 items-center">
-                <div>
-                    <span className="text-sm font-medium mb-1 block">{t('ideascaleProfiles.limitFunds')}</span>
-                    <SearchSelect
-                        key="funds"
-                        domain="funds"
-                        selected={filters.funds.map(fund => fund.value)}
-                        onChange={(value) => console.log('Selected Funds:', value)}
-                        placeholder="Select"
-                        multiple={false}
-                    />
-                </div>
+        <>
+            <div className="container w-full rounded-xl bg-background p-4 shadow-md">
+                <div className="grid grid-cols-2 items-center gap-4 lg:grid-cols-5 lg:gap-8">
+                    <div>
+                        <span className="mb-1 block text-sm font-medium">
+                            {t('ideascaleProfiles.limitFunds')}
+                        </span>
+                        <SearchSelect
+                            key="funds"
+                            domain="funds"
+                            selected={ideascaleFilters.funds.map(
+                                (fund) => fund.value,
+                            )}
+                            onChange={(value) =>
+                                console.log('Selected Funds:', value)
+                            }
+                            placeholder="Select"
+                            multiple={false}
+                        />
+                    </div>
 
-                <div>
-                    <span className="text-sm font-medium mb-1 block">{t('ideascaleProfiles.projectStatus')}</span>
-                    <Selector
-                        isMultiselect={true}
-                        options={[
-                            { value: 'complete', label: 'Complete' },
-                            { value: 'in_progress', label: 'In Progress' },
-                            { value: 'unfunded', label: 'Unfunded' },
-                        ]}
-                        setSelectedItems={setProjectStatus}
-                        selectedItems={projectStatus}
-                    />
-                </div>
+                    <div>
+                        <span className="mb-1 block text-sm font-medium">
+                            {t('ideascaleProfiles.projectStatus')}
+                        </span>
+                        <Selector
+                            isMultiselect={true}
+                            options={[
+                                { value: 'complete', label: 'Complete' },
+                                { value: 'in_progress', label: 'In Progress' },
+                                { value: 'unfunded', label: 'Unfunded' },
+                            ]}
+                            setSelectedItems={setProjectStatus}
+                            selectedItems={projectStatus}
+                        />
+                    </div>
 
-                <div>
-                    <span className="text-sm font-medium mb-1 block">{t('ideascaleProfiles.limitTags')}</span>
-                    <SearchSelect
-                        key="tags"
-                        domain="tags"
-                        selected={filters.tags.map(tag => tag.value)}
-                        onChange={(value) => console.log('Selected Tags:', value)}
-                        placeholder="Select Tags"
-                        multiple={true}
-                    />
-                </div>
+                    <div>
+                        <span className="mb-1 block text-sm font-medium">
+                            {t('ideascaleProfiles.limitTags')}
+                        </span>
+                        <SearchSelect
+                            key="tags"
+                            domain="tags"
+                            selected={ideascaleFilters.tags.map(
+                                (tag) => tag.value,
+                            )}
+                            onChange={(value) =>
+                                console.log('Selected Tags:', value)
+                            }
+                            placeholder="Select Tags"
+                            multiple={true}
+                        />
+                    </div>
 
-                <FundingStatusToggle
-                    checked={fundingStatus}
-                    onChange={handleFundingStatusChange}
-                />
-
-                <div className="col-span-2 sm:col-span-2 lg:col-span-1">
-                    <RangePicker
-                        key="budgets"
-                        context="Budgets"
-                        value={budgetRange}
-                        onValueChange={setBudgetRange}
-                        max={10000000}
-                        min={1000}
-                        defaultValue={[1000, 10000000]}
+                    <FundingStatusToggle
+                        checked={fundingStatus}
+                        onChange={handleFundingStatusChange}
                     />
+
+                    <div className="col-span-2 sm:col-span-2 lg:col-span-1">
+                        <RangePicker
+                            key="budgets"
+                            context="Budgets"
+                            value={budgetRange}
+                            onValueChange={setBudgetRange}
+                            max={10000000}
+                            min={1000}
+                            defaultValue={[1000, 10000000]}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className='w-full'>
+                <IdeascaleProfilesSearchControls />
+            </div>
+        </>
     );
 }
