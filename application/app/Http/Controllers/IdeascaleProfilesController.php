cf39c95ec569c6b45c047ff0d9e67b4a380dcad1
Cindy <cindyjoan2000@gmail.com>
2025-01-06 13:53:41 +0300

    Search function for the people's page

--- application/app/Http/Controllers/IdeascaleProfilesController.php
+++ application/app/Http/Controllers/IdeascaleProfilesController.php
@@ -1,9 +1,13 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
 use App\Repositories\IdeascaleProfileRepository;
+use App\Enums\ProposalSearchParams;
 use Illuminate\Http\Request;
+use Laravel\Scout\Builder;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -12,28 +16,53 @@ class IdeascaleProfilesController extends Controller
     /**
      * Display the user's profile form.
      */
-    protected int $limit = 36;
+    protected int $limit = 40;
+
+    protected array $queryParams = [];
 
 
     public function index(Request $request): Response
     {
-        $ideascaleProfiles = $this->getIdeascaleProfilesData();
+        $this->getProps($request);
 
+        $ideascaleProfiles = empty($this->queryParams) ? $this->getIdeascaleProfilesData() : $this->query();
         $ideascaleProfiles = $ideascaleProfiles->map(function ($ideascaleProfile) {
             $ideascaleProfile->own_proposals_count = $ideascaleProfile->own_proposals->count();
             $ideascaleProfile->co_proposals_count = $ideascaleProfile->co_proposals_count;
-
             return $ideascaleProfile;
         });
 
         return Inertia::render('IdeascaleProfile/Index', [
             'ideascaleProfiles' => $ideascaleProfiles,
+            'filters' => $this->queryParams
         ]);
     }
+
+
 
     public function getIdeascaleProfilesData()
     {
         $ideascaleProfiles = app(IdeascaleProfileRepository::class);
+
         return $ideascaleProfiles->getQuery()->inRandomOrder()->limit($this->limit)->get();
+    }
+
+    protected function getProps(Request $request): void
+    {
+        $this->queryParams = $request->validate([
+            ProposalSearchParams::QUERY()->value => 'string|nullable',
+        ]);
+    }
+
+    protected function query($returnBuilder = false, $attrs = null, $filters = [])
+    {
+        $ideascaleProfiles = app(IdeascaleProfileRepository::class);
+        $builder = $ideascaleProfiles->search(
+            $this->queryParams[ProposalSearchParams::QUERY()->value] ?? '',
+        );
+
+        $response = $builder->get();
+
+        return $response;
     }
 }
---
+++ application/resources/js/Pages/IdeascaleProfile/Partials/IdeascaleProfileSearchControls.tsx
@@ -0,0 +1,40 @@
+import SearchBar from '@/Components/SearchBar';
+import Selector from '@/Components/Select';
+import { useFilterContext } from '@/Context/FiltersContext';
+import { ProposalParamsEnum } from '@/enums/proposal-search-params';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ProposalSearchParams } from '../../../../types/proposal-search-params';
+
+function IdeascaleProfilesSearchControls() {
+    const { filters, setFilters } = useFilterContext<ProposalSearchParams>();
+    const { t } = useTranslation();
+
+    const queryParams = new URLSearchParams(window.location.search);
+    const initialSearchQuery = queryParams.get(ProposalParamsEnum.QUERY) || '';
+    const [searchQuery, setSearchQuery] = useState(initialSearchQuery);
+
+    useEffect(() => {
+        setSearchQuery(initialSearchQuery);
+    }, [initialSearchQuery]);
+
+    const handleSearch = (search: string) => {
+        setFilters(ProposalParamsEnum.QUERY, search);
+        setSearchQuery(search);
+    };
+    
+    return (
+        <div className="mx-auto flex w-full flex-col gap-4 bg-background-lighter pb-4 pt-6">
+            <div className="flex items-center justify-end gap-2">
+                <SearchBar
+                    handleSearch={handleSearch}
+                    autoFocus
+                    showRingOnFocus
+                    initialSearch={searchQuery}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default  IdeascaleProfilesSearchControls;
