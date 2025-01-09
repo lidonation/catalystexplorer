fe80e8392381f412f16230f836ab4e7c60242f13
Darlington Kofa <darlington@lidonation.com>
2025-01-09 17:23:33 +0300

    fix: mainnet ingressroute was being ignored

--- application/resources/js/Pages/Funds/Fund.tsx
+++ application/resources/js/Pages/Funds/Fund.tsx
@@ -11,9 +11,14 @@ export default function Fund({ fund }: PageProps<FundPageProps>) {
 
     return (
         <>
-            <Head title={fund.title} />
+            <Head title={fund.title}/>
+
             <div className="container">
                 <h1 className="title-1">{fund.title}</h1>
+            </div>
+
+            <div className="container flex flex-col h-screen w-full items-center justify-center">
+                <h1>{t('comingSoon')}</h1>
             </div>
         </>
     );
