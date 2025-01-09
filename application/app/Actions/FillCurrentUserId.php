3789c90b6e6db3dd1f33496dcd8f1468834ec2ba
Darlington Kofa <darlington@lidonation.com>
2025-01-09 19:49:14 +0300

    feat(annoucement): add annoucement nova resource

---
+++ application/app/Actions/FillCurrentUserId.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions;
+
+use App\Models\Model;
+
+class FillCurrentUserId
+{
+    public function __invoke(Model &$model): void
+    {
+        $this->handle($model);
+    }
+
+    public function handle(Model &$model): void
+    {
+        // Fill the current user id
+        $model->user_id = auth()->id();
+    }
+}
