65f07e9fbdd004772c1c8e964062e5c60a9f0dce
njuma <Nahshonjumaz@gmail.com>
2025-01-09 17:53:08 +0300

    fix(FundCard):ln-1383 update FundCard for mobile view

--- application/resources/js/Pages/Funds/Partials/FundCard.tsx
+++ application/resources/js/Pages/Funds/Partials/FundCard.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import ArrowTrendingDown from "@/Components/svgs/ArrowTrendingDown";
 import ArrowTrendingUp from "@/Components/svgs/ArrowTrendingUp";
-import FundData = App.DataTransferObjects.FundData;
 
 interface FundCardProps {
-    fund: FundData;
+    fund: any;
     totalAllocated: number;
     totalBudget: number;
     fundedProjects: number;
@@ -38,64 +37,69 @@ const FundCard: React.FC<FundCardProps> = ({
     const formattedProjectPercentageChange = `${projectPercentageChange > 0 ? "+" : ""}${projectPercentageChange}%`;
 
     return (
-        <div className="bg-background rounded-lg shadow-md p-4 sm:p-6 flex flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-4 w-full">
-            {/* Title and Image Section */}
-            <div className="flex-none flex flex-col items-center sm:items-start space-y-4 w-full sm:w-1/2 flex-shrink-0 sm:flex-grow-0">
-                <h2 className="text-lg sm:text-xl font-bold text-center sm:text-left truncate">
+        <div className="bg-background rounded-lg shadow-md p-3 sm:p-4 flex flex-row space-x-4 items-stretch w-full overflow-hidden">
+            {/* Image Section */}
+            <div className="flex-none flex flex-col items-center sm:items-start space-y-2 sm:space-y-4">
+                {/* Title */}
+                <h2 className="text-base sm:text-xl font-bold text-center sm:text-left truncate">
                     {fund.title}
                 </h2>
-                <div className="w-24 h-24 sm:w-36 sm:h-36 rounded-full bg-gradient-to-r from-gray-100 to-gray-900 flex items-center justify-center overflow-hidden">
+
+                {/* Responsive Image Section */}
+                <div className="w-24 h-24 sm:w-32 sm:h-32 lg:w-36 lg:h-36 rounded-full bg-gradient-to-r from-gray-100 to-gray-900 flex items-center justify-center overflow-hidden">
                     <img
-                        src={fund.hero_img_url || "/default-hero-image.jpg"}
+                        src={fund.hero_img_url}
                         alt={fund.title || "Fund"}
                         className="rounded-full w-full h-full object-cover"
                     />
                 </div>
             </div>
 
+
             {/* Details Section */}
-            <div className="flex-grow flex flex-col justify-between space-y-4 overflow-hidden w-full sm:w-1/2 flex-shrink-0">
-                <div className="w-full">
-                    <p className="text-sm truncate mt-6">{t('funds.totalAllocated')}</p>
+            <div className="flex-grow flex flex-col space-y-1 sm:space-y-2">
+                <div>
+                    <p className="text-xs sm:text-sm truncate mt-8">{t('funds.totalAllocated')}</p>
                     <div className="flex items-baseline space-x-1">
-                        <span className="text-lg sm:text-xl font-bold">{formatAmount(totalAllocated)}</span>
-                        <span className="text-sm">/</span>
-                        <span className="text-md text-gray-500 truncate">{formatAmount(totalBudget)} ₳</span>
+                        <span className="text-sm sm:text-base font-bold truncate">
+                            {formatAmount(totalAllocated)}
+                        </span>
+                        <span className="text-xs sm:text-sm">/</span>
+                        <span className="text-xs sm:text-sm text-gray-500 truncate">
+                            {formatAmount(totalBudget)} ₳
+                        </span>
                     </div>
                     <div className="flex items-center mt-1">
                         {isIncrease ? (
-                            <ArrowTrendingUp className="w-4 h-4 text-green-500" />
+                            <ArrowTrendingUp className="w-3 h-3 sm:w-4 sm:h-4 text-green-500" />
                         ) : (
-                            <ArrowTrendingDown className="w-4 h-4 text-red-500" />
+                            <ArrowTrendingDown className="w-3 h-3 sm:w-4 sm:h-4 text-red-500" />
                         )}
-                        <span className="truncate ml-1">{percentageChange}</span>
-                        <span className="ml-1">vs {t('funds.lastFund')}</span>
+                        <span className="ml-1 text-xs sm:text-sm truncate">{percentageChange}</span>
+                        <span className="ml-1 text-xs sm:text-sm truncate">vs {t('funds.lastFund')}</span>
                     </div>
                 </div>
 
-                <div className="w-full">
-                    <p className="text-sm truncate">{t('funds.fundedProjects')}</p>
+                <div>
+                    <p className="text-xs sm:text-sm mt-2 truncate">{t('funds.fundedProjects')}</p>
                     <div className="flex items-baseline space-x-1">
-                        <span className="text-lg sm:text-xl font-bold">{fundedProjects}</span>
-                        <span className="text-sm">/</span>
-                        <span className="text-md text-gray-500 truncate">{totalProjects}</span>
+                        <span className="text-sm sm:text-base font-bold truncate">{fundedProjects}</span>
+                        <span className="text-xs sm:text-sm">/</span>
+                        <span className="text-xs sm:text-sm text-gray-500 truncate">{totalProjects}</span>
                     </div>
                     <div className="flex items-center mt-1">
                         {projectPercentageChange > 0 ? (
-                            <ArrowTrendingUp className="w-4 h-4 text-green-500" />
+                            <ArrowTrendingUp className="w-3 h-3 sm:w-4 sm:h-4 text-green-500" />
                         ) : (
-                            <ArrowTrendingDown className="w-4 h-4 text-red-500" />
+                            <ArrowTrendingDown className="w-3 h-3 sm:w-4 sm:h-4 text-red-500" />
                         )}
-                        <span
-                            className={`${projectPercentageChange > 0 ? "text-green-500" : "text-red-500"} truncate ml-1`}
-                        >
-                            {formattedProjectPercentageChange}
-                        </span>
-                        <span className="ml-1">vs {t('funds.lastFund')}</span>
+                        <span className="ml-1 text-xs sm:text-sm truncate">{formattedProjectPercentageChange}</span>
+                        <span className="ml-1 text-xs sm:text-sm truncate">vs {t('funds.lastFund')}</span>
                     </div>
                 </div>
             </div>
         </div>
+
     );
 };
 
