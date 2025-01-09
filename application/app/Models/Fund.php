59d21c2661671f274d6202a3e216e16c645b5f22
Nashon Juma <nahshonjumaz@gmail.com>
2025-01-09 11:22:25 +0000

    Feature/ln 1383 ce fund card frontend

--- application/app/Models/Fund.php
+++ application/app/Models/Fund.php
@@ -36,7 +36,7 @@ class Fund extends Model implements HasMedia
     public function currencySymbol(): Attribute
     {
         return Attribute::make(
-            get: fn () => match ($this->currency) {
+            get: fn() => match ($this->currency) {
                 CatalystCurrencies::ADA()->value => CatalystCurrencySymbols::ADA,
                 default => CatalystCurrencySymbols::USD
             }
@@ -60,11 +60,11 @@ class Fund extends Model implements HasMedia
     {
         $query->when(
             $filters['search'] ?? false,
-            fn (Builder $query, $search) => $query->where('title', 'ILIKE', '%'.$search.'%')
+            fn(Builder $query, $search) => $query->where('title', 'ILIKE', '%' . $search . '%')
         )->when(
-            $filters['status'] ?? false,
-            fn (Builder $query, $status) => $query->where('status', $status)
-        );
+                $filters['status'] ?? false,
+                fn(Builder $query, $status) => $query->where('status', $status)
+            );
     }
 
     public function proposals(): HasMany
@@ -101,6 +101,11 @@ class Fund extends Model implements HasMedia
     public function registerMediaCollections(): void
     {
         $this->addMediaCollection('hero');
+    }
+
+    public function getHeroImgUrlAttribute()
+    {
+        return $this->getFirstMediaUrl('hero') ?: null;
     }
 
     protected function casts(): array
--- application/database/seeders/FundSeeder.php
+++ application/database/seeders/FundSeeder.php
@@ -1,29 +1,34 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Database\Seeders;
-
+use Carbon\Carbon;
 use App\Models\Fund;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Carbon;
+use Database\Seeders\Traits\GetImageLink;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 
 class FundSeeder extends Seeder
 {
+    use GetImageLink;
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
         $date = Carbon::make(now());
-        Fund::factory()
+        $funds = Fund::factory()
             ->count(5)
             ->recycle(User::factory()->create())
             ->sequence(fn (Sequence $sequence) => [
                 'launched_at' => $date->subMonths($sequence->count + 1),
             ])
             ->create();
+        $funds->each(function (Fund $fund) {
+            if ($imageLink = $this->getRandomImageLink()) {
+                $fund->addMediaFromUrl($imageLink)->toMediaCollection('hero');
+            }
+        });
     }
 }
