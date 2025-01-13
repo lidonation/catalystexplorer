dabf4b7dd09f69a47bb985a1a71b376953d8369a
Cindy Joan <cindyjoan2000@gmail.com>
2025-01-13 10:09:34 +0000

    feat(funds): funds page backend

--- application/app/Models/Fund.php
+++ application/app/Models/Fund.php
@@ -25,12 +25,17 @@ class Fund extends Model implements HasMedia
         'media',
     ];
 
-    protected $appends = [];
+    protected $appends = [
+        'amount_received',
+        'amount_requested'
+    ];
 
     protected $guarded = [];
 
     protected $withCount = [
         'proposals',
+        'fundedProposals',
+        'completedProposals'
     ];
 
     public function currencySymbol(): Attribute
@@ -70,6 +75,34 @@ class Fund extends Model implements HasMedia
     public function proposals(): HasMany
     {
         return $this->hasMany(Proposal::class, 'fund_id', 'id');
+    }
+
+    public function fundedProposals(): HasMany
+    {
+        return $this->hasMany(Proposal::class)->where('funding_status', 'funded');
+    }
+
+    public function completedProposals(): HasMany
+    {
+        return $this->hasMany(Proposal::class)->where('status', 'complete');
+    }
+
+    public function amountReceived(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                return $this->proposals()->sum('amount_received');
+            }
+        );
+    }
+
+    public function amountRequested(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                return $this->proposals()->sum('amount_requested');
+            }
+        );
     }
 
     public function campaigns(): HasMany
