80323852dbfaf3db92d7f90313134fcbe5d26a2d
sawe-daisy <deesawe16@gmail.com>
2025-01-20 12:47:02 +0300

    feat(discussion-backend): feature/LN-1429-ce-Bootstrap-Discussion-backend

---
+++ application/tests/Unit/DiscussionTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Discussion;
+use App\Models\Review;
+use App\Models\Proposal;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class DiscussionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    #[Test]
+    public function it_has_a_review_relationship()
+    {
+
+        $review = Review::factory()->create();
+        $discussion = Discussion::factory()->create([
+            'model_type' => Review::class,
+            'model_id' => $review->id,
+        ]);
+
+        $this->assertInstanceOf(Review::class, $discussion->review);
+        $this->assertEquals($review->id, $discussion->review->id);
+    }
+
+    #[Test]
+    public function it_has_a_proposal_relationship()
+    {
+        $proposal = Proposal::factory()->create();
+        $discussion = Discussion::factory()->create([
+            'model_type' => Proposal::class,
+            'model_id' => $proposal->id,
+        ]);
+
+        $this->assertInstanceOf(Proposal::class, $discussion->proposal);
+        $this->assertEquals($proposal->id, $discussion->proposal->id);
+    }
+
+    #[Test]
+    public function it_correctly_casts_dates()
+    {
+        $discussion = Discussion::factory()->create([
+            'updated_at' => now()->subDays(2),
+            'published_at' => now()->subDays(10),
+        ]);
+
+        $this->assertEquals($discussion->updated_at->format('Y-m-d'), now()->subDays(2)->format('Y-m-d'));
+        $this->assertEquals($discussion->published_at->format('Y-m-d'), now()->subDays(10)->format('Y-m-d'));
+    }
+
+    #[Test]
+    public function it_has_a_polymorphic_model_relationship()
+    {
+        $review = Review::factory()->create();
+        $discussion = Discussion::factory()->create([
+            'model_type' => Review::class,
+            'model_id' => $review->id,
+        ]);
+
+        $this->assertInstanceOf(Review::class, $discussion->model);
+    }
+}
