2cd0685fc74862edcb6ab77f5341c2fcc338b8fc
vwambua <victorwambua004@gmail.com>
2025-01-22 21:57:02 +0300

    feature/ln-1439-bootstrap-catalystregistration-and-delegation-models-architure-update

--- application/app/Models/Delegation.php
+++ application/app/Models/Delegation.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use App\Models\Registration;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Delegation extends Model
