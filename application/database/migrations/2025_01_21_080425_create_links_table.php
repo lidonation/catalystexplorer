84d63253769c69779702447ee0f9ac11a32a192e
Grace Mugoiri <gracemugoiri@gmail.com>
2025-01-22 10:26:16 +0300

    clean up migration files to only have one migration

--- application/database/migrations/2025_01_21_080425_create_links_table.php
+++ application/database/migrations/2025_01_21_080425_create_links_table.php
@@ -4,26 +4,33 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class UpdateLinksTable extends Migration
 {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('links', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string('uri');
-            $table->morphs('model');
+        Schema::table('links', function (Blueprint $table) {
+            $table->string('type')->nullable();
+            $table->text('link')->change();
+            $table->string('label')->nullable();
+            $table->text('title')->nullable();
+            $table->string('status')->default('published');
+            $table->integer('order')->default(0);
+            $table->boolean('valid')->default(true);
+            $table->softDeletes();
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('links');
+        Schema::table('links', function (Blueprint $table) {
+            $table->dropColumn(['type', 'label', 'title', 'status', 'order', 'valid', 'deleted_at']);
+            $table->string('link')->change();
+        });
     }
-};
+}
