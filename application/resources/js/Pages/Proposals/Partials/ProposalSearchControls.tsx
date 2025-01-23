8e9918fc6dbbf07a1ca10935202066c1eef771da
WepngongMaureen <maureenwepngong@gmail.com>
2025-01-23 15:38:52 +0100

    feat: implement frosted background for sticky header on proposals page.

--- application/resources/js/Pages/Proposals/Partials/ProposalSearchControls.tsx
+++ application/resources/js/Pages/Proposals/Partials/ProposalSearchControls.tsx
@@ -45,7 +45,7 @@ function ProposalSearchControls({
     ).length;
 
     return (
-        <div className="container sticky top-0 z-10 mx-auto flex w-full flex-col gap-4 bg-background-lighter pb-4 pt-6">
+        <div className="container sticky top-0 z-10 mx-auto flex w-full flex-col gap-4 pb-4 pt-6 backdrop-blur-md">
             <div className="flex items-center justify-end gap-2">
                 <SearchBar
                     handleSearch={handleSearch}
