f9ae5bbfab0b1adff092af99b2b365a198f03c9c
Grace  Mugoiri <gracemugoiri@Graces-MacBook-Air.local>
2025-01-25 14:33:06 +0300

    comment out eloquent to test out pipeline error

--- application/app/Models/Link.php
+++ application/app/Models/Link.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Models;
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
