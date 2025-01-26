c58c70261d64fa10236ab0aebf6560d84c80c64c
WillardDev <willex.ox@gmail.com>
2025-01-26 14:28:59 +0300

    feat(my.bookmarkspage): ln-1498 made the search work in my bookmarks page.

--- application/resources/js/Pages/My/Bookmarks/Partials/BookmarkToolbar.tsx
+++ application/resources/js/Pages/My/Bookmarks/Partials/BookmarkToolbar.tsx
@@ -1,47 +1,14 @@
-import Button from '@/Components/atoms/Button';
-import Selector from '@/Components/Select';
-import FilterLinesIcon from '@/Components/svgs/FilterLinesIcon';
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import BookmarkSearchControls from './BookmarkSearchControls';
 
-
 const BookmarkToolbar = () => {
-    const [toggleFilterVisibility, setToggleFilterVisibility] = useState(false);
-    const filters = (search: string) => {}
-    const setFilters = (search: string) => {}
     const { t } = useTranslation();
-
-    const sortingOptions: { label: string; value: string; }[] | undefined = [];
 
     return (
         <div className="flex w-full flex-col gap-4">
             <div className="flex flex-row items-center justify-between gap-2">
                 <div className='w-full'>
                     <BookmarkSearchControls/>
-                </div>
-                <div className="flex flex-row gap-2">
-                <Button
-                    className={`shadow-xs border-input flex flex-row items-center gap-2 rounded-lg border bg-background px-2 py-1  ${
-                        toggleFilterVisibility
-                            ? 'border-accent-blue ring-1 ring-offset-background text-primary'
-                            : 'text-gray-500 hover:bg-background-lighter'
-                    }`}
-                    onClick={() =>
-                        setToggleFilterVisibility(!toggleFilterVisibility)
-                    }
-                >
-                    <FilterLinesIcon width={16} />
-                    <span>{t('filters')}</span>
-                </Button>
-                <Selector
-                    isMultiselect={false}
-                    selectedItems={filters}
-                    setSelectedItems={setFilters}
-                    options={sortingOptions}
-                    hideCheckbox={true}
-                    placeholder={t('proposals.options.sort')}
-                />
                 </div>
             </div>
         </div>
