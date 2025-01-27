8f23f1e6cacfe8069e771fc913fcb9097c0f082f
WillardDev <willex.ox@gmail.com>
2025-01-27 18:42:52 +0300

    feat(submitted card): ln-1483 created the submitted card component.

---
+++ application/resources/js/Components/svgs/BackArrow.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const BackArrow: React.FC = () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" className="mr-2">
+        <path fill="currentColor" d="M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z" />
+    </svg>
+);
