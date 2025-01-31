d4fcdd772f0323fd4cc5730dd91bdf726ac66204
Noel Wesley O. <noelwesley8750@gmail.com>
2025-01-31 12:29:25 +0000

    Feature/ln 1493 ce add backend functionality to completed projects nfts

--- application/resources/js/Pages/CompletedProjectNfts/Index.tsx
+++ application/resources/js/Pages/CompletedProjectNfts/Index.tsx
@@ -1,76 +1,123 @@
-import {Head} from "@inertiajs/react";
-import {useTranslation} from "react-i18next";
-import CompletionNftImage from "@/assets/images/project-completion-nfts.jpg";
-import LoginForm from "@/Components/LoginForm";
-import PeopleIcon from "@/Components/svgs/PeopleIcon";
-import FileIcon from "@/Components/svgs/FileIcon";
-import UsdIcon from "@/Components/svgs/UsdIcon";
-import AdaIcon from "@/Components/svgs/AdaIcon";
-import StatisticCard from "./Partials/StatisticCard";
-import CompletedNftsProposalSearchBar from "@/Pages/CompletedProjectNfts/Partials/CompletedNftsProposalSearchBar";
+import CompletionNftImage from '@/assets/images/project-completion-nfts.jpg';
+import LoginForm from '@/Components/LoginForm';
+import AdaIcon from '@/Components/svgs/AdaIcon';
+import FileIcon from '@/Components/svgs/FileIcon';
+import PeopleIcon from '@/Components/svgs/PeopleIcon';
+import UsdIcon from '@/Components/svgs/UsdIcon';
+import CompletedNftsProposalSearchBar from '@/Pages/CompletedProjectNfts/Partials/CompletedNftsProposalSearchBar';
+import { Head, usePage } from '@inertiajs/react';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import StatisticCard from './Partials/StatisticCard';
+import StepTracker from './Partials/StepTracker';
+import { PaginatedData } from '../../../types/paginated-data';
+import { PageProps } from '@/types';
+import ProposalData = App.DataTransferObjects.ProposalData;
+import { ProposalSearchParams } from '../../../types/proposal-search-params';
+import { FiltersProvider} from '@/Context/FiltersContext';
 
-const Index = () => {
-    const {t} = useTranslation();
+interface CompletedProjectNftsPageProps extends Record<string, unknown> {
+    proposals: PaginatedData<ProposalData[]>;
+    filters: ProposalSearchParams;
+}
+
+export default function Index({
+    proposals,
+    filters
+}: PageProps<CompletedProjectNftsPageProps>) {
+
+    const { t } = useTranslation();
+    const { auth } = usePage().props;
+    const user = auth?.user;
+    // Check authentication status
+    const isAuthenticated = !!user;
+
+    const [currentStep, setCurrentStep] = useState<1 | 2 | 3>(isAuthenticated ? 2 : 1);
+
+    useEffect(() => {
+      if(isAuthenticated) {
+        console.log('proposals', JSON.stringify(proposals, null, 2));
+      }
+    },[proposals]);
+
+    useEffect(() => {
+        if (isAuthenticated && currentStep === 1) {
+          setCurrentStep(2);
+        }
+    }, [isAuthenticated]);
+
+    const goToNextStep = (step: 1 | 2 | 3) => {
+        setCurrentStep(step);
+    };
+
+    const goToPreviousStep = (step: 1 | 2 | 3) => {
+        setCurrentStep(step);
+    }
 
     const statistics = [
         {
-            value: "1436",
-            description: t("completedProjectNfts.communityFunded"),
-            icon: <PeopleIcon stroke="#3FACD1"/>
+            value: '1436',
+            description: t('completedProjectNfts.communityFunded'),
+            icon: <PeopleIcon stroke="#3FACD1" />,
         },
         {
-            value: "1064",
-            description: t("completedProjectNfts.projectsCompleted"),
-            icon: <FileIcon/>
+            value: '1064',
+            description: t('completedProjectNfts.projectsCompleted'),
+            icon: <FileIcon />,
         },
         {
-            value: "$35.37M",
-            description: t("completedProjectNfts.usdDistributed"),
-            icon: <UsdIcon/>
+            value: '$35.37M',
+            description: t('completedProjectNfts.usdDistributed'),
+            icon: <UsdIcon />,
         },
         {
-            value: "84.96M ₳",
-            description: t("completedProjectNfts.adaDistributed"),
-            icon: <AdaIcon/>
+            value: '84.96M ₳',
+            description: t('completedProjectNfts.adaDistributed'),
+            icon: <AdaIcon />,
         },
     ];
 
     return (
-        <>
-            <Head title="Charts"/>
+        <FiltersProvider defaultFilters={filters} routerOptions={{ only: ['proposals'] }}>
+            <Head title="Charts" />
 
             {/* Header Section */}
             <header className="py-12">
                 <div className="container mx-auto px-4 sm:px-6">
-                    <h1 className="text-4xl font-bold mb-4">{t("completedProjectNfts.title")}</h1>
-                    <p className="text-lg">{t("completedProjectNfts.subtitle")}</p>
+                    <h1 className="mb-4 text-4xl font-bold">
+                        {t('completedProjectNfts.title')}
+                    </h1>
+                    <p className="text-lg">
+                        {t('completedProjectNfts.subtitle')}
+                    </p>
                 </div>
             </header>
 
             {/* Hero Section */}
             <div className="container mx-auto px-4 sm:px-6">
-                <div className="relative w-full rounded-lg overflow-hidden">
+                <div className="relative w-full overflow-hidden rounded-lg">
                     <img
                         src={CompletionNftImage}
                         alt="Project Catalyst Hero"
-                        className="w-full h-auto object-cover"
+                        className="h-auto w-full object-cover"
                     />
                 </div>
             </div>
 
             {/* Proposals Search Bar */}
-            <div className='container'>
+            <div className="container">
                 <CompletedNftsProposalSearchBar
                     autoFocus={true}
                     showRingOnFocus={true}
-                    handleSearch={(query) => console.log(query)}
+                    //handleSearch={handleSearch}
                     focusState={(isFocused) => console.log(isFocused)}
+                    initialSearch={''}
                 />
             </div>
 
             {/* Statistics Section */}
-            <section className="container mx-auto px-4 sm:px-6 py-12">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+            <section className="container mx-auto px-4 py-12 sm:px-6">
+                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
                     {statistics.map((stat, index) => (
                         <StatisticCard
                             key={index}
@@ -91,9 +138,21 @@ const Index = () => {
                 </div>
             </section>
 
-            <LoginForm title={`${t("completedProjectNfts.nowMinting")}: ${t("funds.funds")} 2-12`}/>
-        </>
+            <div className="container mx-auto px-4 py-12 sm:px-6 w-full flex flex-col items-center justify-center">
+                {currentStep === 1 && (
+                    <LoginForm
+                        title={`${t('completedProjectNfts.nowMinting')}: ${t('funds.funds')} 2-12`}
+                    />
+                )}
+
+                {currentStep === 2 && <p>Form 2</p>}
+
+                {currentStep === 3 && <p>Form 3</p>}
+                
+                <StepTracker totalSteps={3} currentStep={currentStep} />
+            </div>
+
+            
+        </FiltersProvider>
     );
 };
-
-export default Index;
--- application/resources/js/Pages/CompletedProjectNfts/Partials/CompletedNftsProposalSearchBar.tsx
+++ application/resources/js/Pages/CompletedProjectNfts/Partials/CompletedNftsProposalSearchBar.tsx
@@ -1,13 +1,15 @@
 import TextInput from '@/Components/TextInput';
 import SearchLensIcon from '@/Components/svgs/SearchLensIcon';
+import { useFilterContext } from '@/Context/FiltersContext';
 import useEscapeKey from '@/Hooks/useEscapeKey';
+import { ProposalParamsEnum } from '@/enums/proposal-search-params';
 import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 interface SearchBarProps {
     autoFocus?: boolean;
     showRingOnFocus?: boolean;
-    handleSearch: (search: string) => void;
+    //handleSearch: (search: string) => void;
     focusState?: (state: boolean) => void;
     initialSearch?: string;
 }
@@ -15,11 +17,10 @@ interface SearchBarProps {
 const CompletedNftsProposalSearchBar = ({
     autoFocus = false,
     showRingOnFocus = false,
-    handleSearch,
+    //handleSearch,
     focusState,
     initialSearch,
 }: SearchBarProps) => {
-    const [searchQuery, setSearchQuery] = useState(initialSearch);
     const inputRef = useRef<HTMLInputElement>(null);
     const { t } = useTranslation();
 
@@ -34,6 +35,29 @@ const CompletedNftsProposalSearchBar = ({
     const placeholder = t(
         'completedProjectNfts.proposalsSearchBar.placeHolder',
     );
+
+    const {setFilters } = useFilterContext();
+    const queryParams = new URLSearchParams(window.location.search);
+    const initialSearchQuery = queryParams.get(ProposalParamsEnum.QUERY) || '';
+    const [searchQuery, setSearchQuery] = useState(initialSearchQuery);
+
+    const handleSearch = (search: string) => {
+        setSearchQuery(search);
+        const url = new URL(window.location.href);
+
+        if (search.trim() === '') {
+            url.searchParams.delete(ProposalParamsEnum.QUERY);
+        } else {
+            setFilters({
+                param: ProposalParamsEnum.QUERY,
+                value: search,
+                label: 'Search',
+            });
+            url.searchParams.set(ProposalParamsEnum.QUERY, search);
+        }
+
+        window.history.replaceState(null, '', url.toString());
+    };
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newValue = event.target.value;
@@ -57,7 +81,7 @@ const CompletedNftsProposalSearchBar = ({
                     ref={inputRef}
                     placeholder={placeholder}
                     size={placeholder.length}
-                    className={`w-full rounded-lg border-0 bg-background pl-10 text-content shadow-none focus:border-0 focus:border-primary ${showRingOnFocus ? 'focus:ring-2 focus:ring-primary' : 'focus:ring-0'}`}
+                    className={`bg-background text-content focus:border-primary w-full rounded-lg border-0 pl-10 shadow-none focus:border-0 ${showRingOnFocus ? 'focus:ring-primary focus:ring-2' : 'focus:ring-0'}`}
                     value={searchQuery}
                     onChange={handleChange}
                     onFocus={() => {
