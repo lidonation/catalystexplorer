4efd2353e5b44fce8679cc3cc1af89fd20ced6a7
Cindy Joan <cindyjoan2000@gmail.com>
2025-02-03 07:42:48 +0000

    Feature/ln 1498 ce proposal card mini

---
+++ application/resources/js/Pages/Groups/Partials/ProposalMiniCard.tsx
@@ -0,0 +1,56 @@
+import ProposalCardHeader from '@/Pages/Proposals/Partials/ProposalCardHeader';
+import ProposalFundingPercentages from '@/Pages/Proposals/Partials/ProposalFundingPercentages';
+import ProposalFundingStatus from '@/Pages/Proposals/Partials/ProposalFundingStatus';
+import { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import Proposal = App.DataTransferObjects.ProposalData;
+import ProposalUsersMini from './ProposalUsersMini';
+
+interface ProposalMiniCardProps {
+    proposal: Proposal;
+    isHorizontal: boolean;
+}
+export default function ProposalMiniCard({
+    proposal,
+    isHorizontal
+}: ProposalMiniCardProps) {
+    const { t } = useTranslation();
+
+    const [userSelected, setUserSelected] =
+        useState<App.DataTransferObjects.IdeascaleProfileData | null>(null);
+
+    const handleUserClick = useCallback(
+        (user: App.DataTransferObjects.IdeascaleProfileData) =>
+            setUserSelected(user),
+        [],
+    );
+
+    const noSelectedUser = useCallback(() => setUserSelected(null), []);
+    return (
+        <article className="bg-background relative flex h-full flex-col rounded-xl p-4 shadow-lg">
+            <div className="flex h-auto w-full flex-col items-start overflow-hidden rounded-xl">
+                <ProposalCardHeader
+                    proposal={proposal}
+                    userSelected={userSelected}
+                    noSelectedUser={noSelectedUser}
+                    isHorizontal={isHorizontal}
+                />
+            </div>
+            <div className="mt-6" aria-labelledby="funding-heading">
+                <div className="flex gap-2">
+                    <h3 className="font-semibold">{t('funding')}</h3>
+                    <ProposalFundingStatus
+                        funding_status={proposal.funding_status}
+                    />
+                </div>
+                <ProposalFundingPercentages proposal={proposal} />
+            </div>
+            <div className="border-t mt-4 border-t-dark/30">
+                <ProposalUsersMini
+                    users={proposal.users}
+                    onUserClick={handleUserClick}
+                />
+            </div>
+        </article>
+    );
+}
