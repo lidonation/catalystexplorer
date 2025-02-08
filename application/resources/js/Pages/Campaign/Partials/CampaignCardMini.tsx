2bbff798c73f13279eddcc082a0f0bdd04996520
Grace  Mugoiri <gracemugoiri@Graces-MacBook-Air.local>
2025-02-08 10:05:50 +0300

    feat: provide better structure for the whole component

--- application/resources/js/Pages/Campaign/Partials/CampaignCardMini.tsx
+++ application/resources/js/Pages/Campaign/Partials/CampaignCardMini.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FundData = App.DataTransferObjects.FundData;
 import CampaignData = App.DataTransferObjects.CampaignData;
 import { currency } from '@/utils/currency';
 import { useTranslation } from 'react-i18next';
-import SegmentedBar from '@/Pages/IdeascaleProfile/Partials/SegmentedProgressBar';
-import IdeascaleProfileData = App.DataTransferObjects.IdeascaleProfileData;
-import {Link} from "@inertiajs/react";
-import {useLocalizedRoute} from "@/utils/localizedRoute";
+import SegmentedCampaignBar from './SegmentedCampaignBar';
 
 interface CampaignCardMiniProps {
   fund: FundData;
@@ -14,9 +11,7 @@ interface CampaignCardMiniProps {
 }
 
 const CampaignCardMini: React.FC<CampaignCardMiniProps> = ({ fund, campaign }) => {
-  const { t } = useTranslation();
-  const rawData : unknown = fund;
-  const fundData :IdeascaleProfileData = rawData as IdeascaleProfileData;
+  const { t } = useTranslation();fund
 
   const heroImageUrl = fund?.hero_img_url;
 
@@ -30,37 +25,24 @@ const CampaignCardMini: React.FC<CampaignCardMiniProps> = ({ fund, campaign }) =
             className="h-full w-full object-cover font-semibold "
           />
         ) : (
-          <div className="flex bg-primary items-center justify-center h-full w-full">
-            <span className="text-lg text-content-light px-4">{campaign?.label}</span>
+          <div className="flex bg-primary items-center justify-center h-full">
+            <span className="text-lg text-content-light">{t('developers')}</span>
           </div>
         )}
       </div>
-
       <div className="pt-6">
         <h3 className="text-lg font-semibold mb-2 flex items-center justify-between">
-            <Link href={useLocalizedRoute('funds.fund.campaigns.campaign.show', {fund: fund.slug, campaign: campaign.slug})}>{campaign?.title}</Link>
-            <svg width="34" height="34" version="1.1" viewBox="0 0 1200 1200" xmlns="http://www.w3.org/2000/svg">
-                <path d="m400 350c0-27.613 22.387-50 50-50h400c27.613 0 50 22.387 50 50v400c0 27.613-22.387 50-50 50s-50-22.387-50-50v-279.29l-414.64 414.64c-19.527 19.523-51.184 19.523-70.711 0-19.527-19.527-19.527-51.184 0-70.711l414.64-414.64h-279.29c-27.613 0-50-22.387-50-50z"/>
-            </svg>
+            {campaign?.title}
         </h3>
-
         <div className="flex w-full justify-between pt-4 pb-4">
-            <SegmentedBar
-                IdeascaleProfileData = {fundData}
-                CompletedProposalsColor="bg-success"
-                FundedProposalsColor="bg-warning"
-                UnfundedProposalsColor="bg-primary"
-            />
-        </div>
-
-        <p className="text-content-dark opacity-80 mb-4 line-clamp-3">
-          {campaign?.excerpt}
-        </p>
-
+                    <SegmentedCampaignBar
+                        CampaignData = {campaign}
+                        CompletedProposalsColor="bg-success"
+                        FundedProposalsColor="bg-warning"
+                        UnfundedProposalsColor="bg-primary"
+                    />
+                </div>
         <div className="flex gap-2">
-          <p className="bg-background text-content rounded-md border pr-2 pl-2">
-            {t('proposals.proposals')}: {campaign?.proposals_count ?? 0}
-          </p>
           <p className="bg-background text-content rounded-md border pr-2 pl-2">
             {t('proposals.filters.budget')}: {currency(campaign?.amount ?? 0, campaign?.currency?.toUpperCase() ?? 'USD', undefined, 2 )}
           </p>
@@ -70,4 +52,7 @@ const CampaignCardMini: React.FC<CampaignCardMiniProps> = ({ fund, campaign }) =
   );
 };
 
+
+
 export default CampaignCardMini;
+
