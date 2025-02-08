438d2f3e3f2ca836f12afb59c779409b9b3d2b83
Grace  Mugoiri <gracemugoiri@Graces-MacBook-Air.local>
2025-02-08 10:55:05 +0300

    feat: use strict type declaration

--- application/app/Models/Location.php
+++ application/app/Models/Location.php
@@ -1,11 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use App\Traits\HasModel;
 use Database\Factories\LocationFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Location extends Model
 {
