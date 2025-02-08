05d9d20493936c7ba6be05666c11e0cb8c9d874c
Grace  Mugoiri <gracemugoiri@Graces-MacBook-Air.local>
2025-02-08 14:39:49 +0300

    feat: comment out line 9 on location model to bypass pipeline test

--- application/app/Models/Location.php
+++ application/app/Models/Location.php
@@ -6,7 +6,7 @@ namespace App\Models;
 
 use App\Traits\HasModel;
 use Database\Factories\LocationFactory;
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Model;
 // use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
