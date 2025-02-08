107f8a14608c8d3f6026ea4c4a98697372ffd647
Grace  Mugoiri <gracemugoiri@Graces-MacBook-Air.local>
2025-02-08 11:05:16 +0300

    feat: use strict type declaration on HasLink

--- application/app/Models/Location.php
+++ application/app/Models/Location.php
@@ -7,13 +7,13 @@ namespace App\Models;
 use App\Traits\HasModel;
 use Database\Factories\LocationFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
 class Location extends Model
 {
     /** @use HasFactory<LocationFactory> */
-    use HasFactory;
+    // use HasFactory;
 
     use HasModel;
 }
