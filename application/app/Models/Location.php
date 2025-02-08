4632e358b5dc5c2d30436100ea32ccc1de332e2d
Victor Wambua <victorwambua004@gmail.com>
2025-02-08 09:23:37 +0000

    feature/ln-1452-implement-404-page-update

--- application/app/Models/Location.php
+++ application/app/Models/Location.php
@@ -1,16 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use App\Traits\HasModel;
-use Database\Factories\LocationFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    /** @use HasFactory<LocationFactory> */
-    use HasFactory;
-
     use HasModel;
 }
