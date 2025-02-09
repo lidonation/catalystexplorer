0aa97b794f0f15b4e9fb5022dfb9c3fc673e040b
sawe-daisy <deesawe16@gmail.com>
2025-02-09 16:10:43 +0300

    clean up

--- application/routes/api.php
+++ application/routes/api.php
@@ -5,6 +5,7 @@ use App\Interfaces\Http\Controllers\Api\CommunityController;
 use App\Interfaces\Http\Controllers\Api\GroupController;
 use App\Interfaces\Http\Controllers\Api\IdeascaleProfilesController;
 use App\Interfaces\Http\Controllers\Api\TagController;
+use App\Interfaces\Http\Controllers\My\MyBookmarksController;
 use App\Interfaces\Http\Controllers\ProposalsController;
 use Illuminate\Support\Facades\Route;
 
