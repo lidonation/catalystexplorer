1e2ee06342632fca46c712db1d6696c9a830bd8d
sawe-daisy <deesawe16@gmail.com>
2025-02-09 16:34:25 +0300

    remove unused import

--- application/resources/js/Hooks/useBookmark.ts
+++ application/resources/js/Hooks/useBookmark.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
-import {Ziggy} from "@/ziggy";
 
 interface UseBookmarkProps {
     modelType: string;
