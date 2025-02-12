fab1828050b29832134e21d0df9c34bfbf74912a
Grace Mugoiri <gracemugoiri@gmail.com>
2025-02-12 08:54:56 +0000

    Feature/ln 1510 ce campaign card mini component updated

--- application/app/Models/Location.php
+++ application/app/Models/Location.php
@@ -2,12 +2,21 @@
 
 declare(strict_types=1);
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use App\Traits\HasModel;
+use Database\Factories\LocationFactory;
+// use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Location extends Model
 {
+    /** @use HasFactory<LocationFactory> */
+    // use HasFactory;
+
     use HasModel;
 
     protected $hidden = ['id', 'pivot.location_id'];
