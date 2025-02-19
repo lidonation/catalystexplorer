b69cf867f39661effdaf5c52c8121199af3782a2
Grace Mugoiri <gracemugoiri@gmail.com>
2025-02-19 16:40:59 +0000

    Feature/ln 1554 ce implement ideascale profile communities page

--- application/resources/js/Pages/My/Proposals/Index.tsx
+++ application/resources/js/Pages/My/Proposals/Index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Head } from '@inertiajs/react';
 import { useTranslation } from 'react-i18next';
 import MyLayout from "@/Pages/My/MyLayout";
+import RecordsNotFound from '@/Layouts/RecordsNotFound';
 
 interface MyProposalsProps {
     notSureWhatThisIs?: any[];
@@ -16,7 +17,7 @@ export default function MyProposals({}: MyProposalsProps) {
 
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
                 <div className="text-center text-content">
-                    {t('my.proposals')} {t('comingSoon')}
+                    <RecordsNotFound />
                 </div>
             </div>
         </MyLayout>
