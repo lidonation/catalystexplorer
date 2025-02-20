41eb8a034979469f49df0f2fbec47728575efb0a
WillardDev <willex.ox@gmail.com>
2025-02-20 12:34:12 +0300

    fix(single group page): ln-1444 made the tabs have their own routes

---
+++ application/resources/js/Pages/Groups/Reviews/Index.tsx
@@ -0,0 +1,33 @@
+import { Head, WhenVisible } from '@inertiajs/react';
+import MyLayout from "../MyLayout";
+import ReviewData = App.DataTransferObjects.ReviewData;
+import GroupData = App.DataTransferObjects.GroupData;
+import { PaginatedData } from '../../../../types/paginated-data';
+import AggregatedReviewsSummary from '@/Components/AggregatedReviewsSummary';
+
+interface ReviewPageProps {
+    reviews: PaginatedData<ReviewData[]>;
+    group: GroupData;
+}
+
+export default function Reviews({ reviews, group }: ReviewPageProps) {
+    return (
+        <MyLayout group={group}>
+            <Head title={`${group.name} - Connections`} />
+            
+            <WhenVisible data='reviews' fallback={<div>Loading Reviews</div>}>
+                <div className='w-full overflow-auto'>
+                    <div>
+                        <AggregatedReviewsSummary reviews={reviews?.data ?? []} />
+                    </div>
+
+                    {typeof reviews?.data !== 'undefined' && (
+                        <div className='max-w-full lg:max-w-lg'>
+                            {JSON.stringify(reviews?.data)}
+                        </div>
+                    )}
+                </div>
+            </WhenVisible>
+        </MyLayout>
+    );
+}
