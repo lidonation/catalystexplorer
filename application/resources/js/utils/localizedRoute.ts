0251dcc808b338bc67360383e43536eaa0f03fec
sawe-daisy <deesawe16@gmail.com>
2025-02-20 15:29:27 +0300

    feat(redirection): use of the localized routes

--- application/resources/js/utils/localizedRoute.ts
+++ application/resources/js/utils/localizedRoute.ts
@@ -7,3 +7,10 @@ export function useLocalizedRoute(name: string, params?: Record<string, any>) {
 
     return route(`${locale || 'en'}.${name}`, params);
 }
+export function generateLocalizedRoute(
+    name: string,
+    params?: Record<string, any>,
+    locale: string = 'en',
+) {
+    return route(`${locale}.${name}`, params);
+}
