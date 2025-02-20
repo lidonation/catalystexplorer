4ba3de8988aa90caa2db7cd9b4e4c1de03fbb61c
Darlington Kofa <darlington@lidonation.com>
2025-02-20 14:43:57 +0300

    chore: cleanup

--- application/resources/js/Pages/Groups/GroupLayout.tsx
+++ application/resources/js/Pages/Groups/GroupLayout.tsx
@@ -1,30 +1,30 @@
-// MyLayout.tsx
-import React, { ReactNode, useEffect, useState } from 'react';
-import { usePage } from '@inertiajs/react';
+import React, {ReactNode, useEffect, useState} from 'react';
+import {usePage} from '@inertiajs/react';
 import Image from '@/Components/Image';
 import Title from '@/Components/atoms/Title';
 import GroupSocials from './Partials/GroupSocials';
 import GroupHeader from '@/assets/images/group-header.jpg';
 import GroupTabs from './Partials/GroupTab';
-import { generateTabs, groupTabs } from '@/utils/routeTabs';
-import { useTranslation } from 'react-i18next';
+import {generateTabs, groupTabs} from '@/utils/routeTabs';
+import {useTranslation} from 'react-i18next';
 import GroupData = App.DataTransferObjects.GroupData;
-import { PageProps as InertiaPageProps } from '@inertiajs/core';
+import {PageProps as InertiaPageProps} from '@inertiajs/core';
 import BioCard from '@/Pages/Groups/Partials/BioCard';
 
 interface PageProps extends InertiaPageProps {
     url: string;
+
     [key: string]: any;
 }
 
-interface MyLayoutProps {
+interface GroupLayoutProps {
     children: ReactNode;
     group: GroupData;
 }
 
-export default function GroupLayout({ children, group }: MyLayoutProps) {
-    const { t } = useTranslation();
-    const { url } = usePage<PageProps>().props;
+export default function GroupLayout({children, group}: GroupLayoutProps) {
+    const {t} = useTranslation();
+    const {url} = usePage<PageProps>().props;
     const [activeTab, setActiveTab] = useState('');
 
     const tabConfig = {
@@ -67,25 +67,31 @@ export default function GroupLayout({ children, group }: MyLayoutProps) {
                             <Title className="text-xl sm:text-2xl font-bold text-content drop-shadow-lg mb-2">
                                 {group.name}
                             </Title>
-                            <GroupSocials group={group} />
+                            <GroupSocials group={group}/>
                         </div>
                     </div>
                 </div>
             </header>
 
-            <section className="px-8 sm:px-4 md:px-6 lg:px-8 mt-10 flex flex-col lg:flex-row gap-4">
+            <main className="px-8 sm:px-4 md:px-6 lg:px-8 mt-10 flex flex-col lg:flex-row gap-4">
                 <div className="w-full md:w-3/4 lg:w-1/3 xl:w-1/4 mx-auto lg:mx-0 lg:sticky lg:top-4 lg:self-start">
-                    <BioCard group={group} />
+                    <BioCard group={group}/>
                 </div>
 
                 <div className="flex flex-col gap-8 w-full lg:w-2/3 xl:w-3/4 shadow-xl bg-background p-4 rounded-lg">
-                    <GroupTabs
-                        tabs={tabs}
-                        activeTab={activeTab}
-                    />
-                    {children}
+                    <section
+                        className=" text-content-lighter [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] overflow-x-auto">
+                        <GroupTabs
+                            tabs={tabs}
+                            activeTab={activeTab}
+                        />
+                    </section>
+
+                    <section>
+                        {children}
+                    </section>
                 </div>
-            </section>
+            </main>
         </div>
     );
 }
--- application/resources/js/Pages/Groups/Partials/GroupTab.tsx
+++ application/resources/js/Pages/Groups/Partials/GroupTab.tsx
@@ -1,39 +1,38 @@
-import { Link } from '@inertiajs/react';
-import type { Tab } from '@/utils/routeTabs';
+import {Link} from '@inertiajs/react';
+import type {Tab} from '@/utils/routeTabs';
 
 interface GroupTabProps {
     tabs: Tab[];
     activeTab: string;
 }
 
-export default function GroupTabs({ tabs, activeTab }: GroupTabProps) {
+export default function GroupTabs({tabs, activeTab}: GroupTabProps) {
     return (
-        <div className=" text-content-lighter [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] overflow-x-auto">
-            <nav className="min-w-max border-b border-b-light-gray-persist">
-                <div className="flex flex-row gap-2 md:gap-6">
-                    {tabs.map((tab) => {
-                        const isActive = activeTab === tab.name;
+        <nav className="min-w-max border-b border-b-light-gray-persist">
+            <div className="flex flex-row gap-2 md:gap-6">
+                {tabs.map((tab) => {
+                    const isActive = activeTab === tab.name;
 
-                        return (
-                            <Link
-                                key={tab.routeName}
-                                href={tab.href}
-                                className={`
+                    return (
+                        <Link
+                            key={tab.routeName}
+                            href={tab.href}
+                            only={[...(tab.only ?? [])]}
+                            className={`
                                     whitespace-nowrap text-sm md:text-base px-2 md:px-3 py-2
                                     group flex items-center gap-2 outline-hidden transition-colors
                                     hover:text-content-dark
-                                    ${isActive 
-                                        ? '-mb-px border-b-2 border-b-primary text-primary'
-                                        : ''
-                                    }
+                                    ${isActive
+                                ? '-mb-px border-b-2 border-b-primary text-primary'
+                                : ''
+                            }
                                 `}
-                            >
-                                {tab.name}
-                            </Link>
-                        );
-                    })}
-                </div>
-            </nav>
-        </div>
+                        >
+                            {tab.name}
+                        </Link>
+                    );
+                })}
+            </div>
+        </nav>
     );
 }
