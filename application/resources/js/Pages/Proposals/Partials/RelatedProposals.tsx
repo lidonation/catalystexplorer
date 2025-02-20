b025db73c35f79033add9a60f27e9d5b0ca888a1
Willard Odongo <willard.odongo10@gmail.com>
2025-02-20 13:58:21 +0000

    Feature/ln 1444 singlegroup page

--- application/resources/js/Pages/Proposals/Partials/RelatedProposals.tsx
+++ application/resources/js/Pages/Proposals/Partials/RelatedProposals.tsx
@@ -1,53 +1,59 @@
 import React, {HTMLAttributes} from 'react';
-import {Link} from '@inertiajs/react';
-import {useTranslation} from 'react-i18next';
+import { Link, WhenVisible } from '@inertiajs/react';
+import { useTranslation } from 'react-i18next';
 import ProposalCardMini from './ProposalCardMini';
+import ProposalMiniCardLoader from './ProposalMiniCardLoader';
 import {PaginatedData} from "../../../../types/paginated-data";
 import ProposalData = App.DataTransferObjects.ProposalData;
+import { useLocalizedRoute } from '@/utils/localizedRoute';
 
 interface RelatedProposalsProps extends HTMLAttributes<HTMLDivElement> {
     proposals: PaginatedData<ProposalData[]>;
     groupId?: string;
-    proposalWrapperClassName?: string;
     maxVisibleProposals?: number;
+    proposalWrapperClassName?: string;
 }
 
 const RelatedProposals: React.FC<RelatedProposalsProps> = ({
-   proposals,
-   groupId,
-   proposalWrapperClassName = '',
-   ...props
+    proposals,
+    groupId,
+    proposalWrapperClassName,
+    ...props
 }) => {
-    const {t} = useTranslation();
+    const { t } = useTranslation();
+
     const showViewMore = proposals.total > proposals.per_page;
 
     return (
-        <div {...props}>
-            {typeof proposals.data !== 'undefined' && (proposals.data.map((proposal) => (
-                <div className={proposalWrapperClassName}>
+        <WhenVisible
+            fallback={<ProposalMiniCardLoader />}
+            data="proposals"
+        >
+            <div {...props}>
+                {typeof proposals.data !== 'undefined' && (proposals.data.map((proposal) => (
                     <ProposalCardMini
                         key={proposal.hash}
                         proposal={proposal}
                         isHorizontal={false}
                     />
-                </div>
-            )))}
+                )))}
 
-            {showViewMore && (
-                <Link
-                    href={`/proposals${groupId ? `?g%5B0%5D=${groupId}` : ''}`}
-                    className="bg-background flex h-full flex-col items-center justify-center rounded-xl p-4 shadow-lg transition-transform hover:scale-95"
-                >
-                    <div className="flex flex-col items-center gap-4">
-                        <div className="text-center">
-                            <p className="text-sm text-gray-600">{t('seeAll')}</p>
-                            <p className="text-xl font-semibold">{proposals.total}</p>
-                            <p className="text-sm text-gray-600">{t('proposals.proposals')}</p>
+                {showViewMore && (
+                    <Link
+                        href={useLocalizedRoute('groups.proposals', { group: groupId })}
+                        className="bg-background flex h-full flex-col items-center justify-center rounded-xl p-4 shadow-lg transition-transform hover:scale-95"
+                    >
+                        <div className="flex flex-col items-center gap-4">
+                            <div className="text-center">
+                                <p className="text-sm text-gray-600">{t('seeAll')}</p>
+                                <p className="text-xl font-semibold">{proposals.total}</p>
+                                <p className="text-sm text-gray-600">{t('proposals.proposals')}</p>
+                            </div>
                         </div>
-                    </div>
-                </Link>
-            )}
-        </div>
+                    </Link>
+                )}
+            </div>
+        </WhenVisible>
     );
 };
 
