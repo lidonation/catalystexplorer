e88ad6bdca013b8d58f6e69c7e0a1ab190e94c98
Darlington Kofa <darlington@lidonation.com>
2025-02-22 18:00:44 +0300

    chore: cleanup

--- application/config/filesystems.php
+++ application/config/filesystems.php
@@ -56,6 +56,9 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => true,
             'scheme' => 'http',
+            'http' => [
+                'verify' => false,
+            ],
         ],
 
     ],
