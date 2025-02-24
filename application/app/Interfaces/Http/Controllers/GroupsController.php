bf666d1fc0080dba25e2e3ad807a3a0a9d11c8de
etiti <emmanueltiti370@gmail.com>
2025-02-24 11:35:34 +0300

    chore: adding full seeders for proposals

--- application/app/Interfaces/Http/Controllers/GroupsController.php
+++ application/app/Interfaces/Http/Controllers/GroupsController.php
@@ -16,6 +16,7 @@ use App\Repositories\GroupRepository;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Fluent;
 use Illuminate\Support\Stringable;
 use Inertia\Inertia;
@@ -110,23 +111,33 @@ class GroupsController extends Controller
 
     public function group(Request $request, Group $group, GroupRepository $groupRepository): Response
     {
-        $group->load(['proposals'])
-            ->loadCount([
-                'proposals',
-                'funded_proposals',
-                'unfunded_proposals',
-                'completed_proposals',
-            ])->append(
-                [
-                    'amount_awarded_ada',
-                    'amount_awarded_usd',
-                    'amount_requested_ada',
-                    'amount_requested_usd',
-                    'amount_distributed_ada',
-                    'amount_distributed_usd',
-                    'connected_items',
-                ]
-            );
+        $startTime = microtime(true); // Start time
+
+        // Load relationships and counts
+        $group->load(['proposals']);
+        $loadProposalsTime = microtime(true);
+        Log::info('Loaded proposals: '.($loadProposalsTime - $startTime).' sec');
+
+        $group->loadCount([
+            'proposals',
+            'funded_proposals',
+            'unfunded_proposals',
+            'completed_proposals',
+        ]);
+        $loadCountsTime = microtime(true);
+        Log::info('Loaded proposal counts: '.($loadCountsTime - $loadProposalsTime).' sec');
+
+        $group->append([
+            'amount_awarded_ada',
+            'amount_awarded_usd',
+            'amount_requested_ada',
+            'amount_requested_usd',
+            'amount_distributed_ada',
+            'amount_distributed_usd',
+            'connected_items',
+        ]);
+        $appendAttributesTime = microtime(true);
+        Log::info('Appended attributes: '.($appendAttributesTime - $loadCountsTime).' sec');
 
         $path = $request->path();
         $connections = $group->connected_items;
@@ -136,6 +147,8 @@ class GroupsController extends Controller
             $proposalsPaginator = $group->proposals()
                 ->with(['users', 'fund'])
                 ->paginate(5);
+            $proposalsQueryTime = microtime(true);
+            Log::info('Fetched proposals: '.($proposalsQueryTime - $appendAttributesTime).' sec');
 
             return Inertia::render('Groups/Proposals/Index', [
                 'group' => GroupData::from($group),
@@ -159,45 +172,58 @@ class GroupsController extends Controller
         }
 
         if (str_contains($path, '/ideascale-profiles')) {
+            $profilesQueryTime = microtime(true);
+            Log::info('Fetched IdeaScale profiles: '.($profilesQueryTime - $appendAttributesTime).' sec');
+
             return Inertia::render('Groups/IdeascaleProfiles/Index', [
                 'group' => GroupData::from($group),
                 'ideascaleProfiles' => Inertia::optional(fn () => to_length_aware_paginator(
                     IdeascaleProfileData::collect(
-                        $group->ideascale_profiles()->with([])->paginate(12)
+                        $group->ideascale_profiles()->paginate(12)
                     )
-                )
-                ),
+                )),
             ]);
         }
 
         if (str_contains($path, '/reviews')) {
+            $reviewsQueryTime = microtime(true);
+            Log::info('Fetched reviews: '.($reviewsQueryTime - $appendAttributesTime).' sec');
+
             return Inertia::render('Groups/Reviews/Index', [
                 'group' => GroupData::from($group),
                 'reviews' => Inertia::optional(fn () => to_length_aware_paginator(
                     ReviewData::collect(
                         Review::query()->paginate(8)
                     )
-                )
-                ),
+                )),
             ]);
         }
 
         if (str_contains($path, '/locations')) {
+            $locationsQueryTime = microtime(true);
+            Log::info('Fetched locations: '.($locationsQueryTime - $appendAttributesTime).' sec');
+
             return Inertia::render('Groups/Locations/Index', [
                 'group' => GroupData::from($group),
                 'locations' => Inertia::optional(fn () => to_length_aware_paginator(
                     LocationData::collect(
                         $group->locations()->paginate(12)
                     )
-                )
-                ),
+                )),
             ]);
         }
 
         // Default return if no specific path matches
         $proposalsPaginator = $group->proposals()
-            ->with(['users', 'fund'])
+            ->select(['id', 'title', 'fund_id', 'campaign_id']) // Only select necessary fields
+            ->with([
+                'users:id,name',
+                'fund:id,title',
+            ])
             ->paginate(5);
+
+        $defaultProposalsQueryTime = microtime(true);
+        Log::info('Fetched default proposals: '.($defaultProposalsQueryTime - $appendAttributesTime).' sec');
 
         return Inertia::render('Groups/Proposals/Index', [
             'group' => GroupData::from($group),
--- application/database/seeders/ProposalSeeder.php
+++ application/database/seeders/ProposalSeeder.php
@@ -13,6 +13,7 @@ use App\Models\Meta;
 use App\Models\Proposal;
 use App\Models\Tag;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Concurrency;
 
 class ProposalSeeder extends Seeder
 {
@@ -21,6 +22,7 @@ class ProposalSeeder extends Seeder
      */
     public function run(): void
     {
+
         $startDate = now()->subYears(3);
 
         $funds = Fund::factory()
@@ -36,28 +38,39 @@ class ProposalSeeder extends Seeder
             fn ($fund) => Campaign::factory()->count(5)->for($fund, 'fund')->create()
         );
 
-        $groups = Group::factory()->count(1200)->create();
+        $groups = Group::factory()->count(30)->create();
         $tags = Tag::factory()->count(25)->create();
-        $communities = Community::factory()->count(1000)->create();
-        $IdeascaleProfiles = IdeascaleProfile::factory()->count(2000)->create();
+        $communities = Community::factory()->count(30)->create();
+        $IdeascaleProfiles = IdeascaleProfile::factory()->count(1000)->create();
 
-        $campaigns->each(fn ($campaign) => dispatch_sync(function () use ($campaign, $communities, $groups, $tags, $IdeascaleProfiles) {
-            Proposal::factory()
-                ->count(130)
-                ->state([
-                    'campaign_id' => $campaign->id,
-                    'fund_id' => $campaign->fund->id,
-                ])
-                ->has(Meta::factory()->state(fn () => [
-                    'key' => fake()->randomElement(['woman_proposal', 'impact_proposal', 'ideafest_proposal']),
-                    'content' => fake()->randomElement([0, 1]),
-                    'model_type' => Proposal::class,
-                ]))
-                ->hasAttached($IdeascaleProfiles->random(fake()->randomElement([1, 3, 4, 5, 8, 10])), relationship: 'users')
-                ->hasAttached($groups->random())
-                ->hasAttached($tags->random(), ['model_type' => Proposal::class])
-                ->hasAttached($communities->random(), [])
-                ->create();
-        }));
+        $campaigns->each(
+            function ($campaign) use ($communities, $groups, $tags, $IdeascaleProfiles) {
+                // Create proposals first
+                $proposals = Proposal::factory()
+                    ->count(50)
+                    ->state([
+                        'campaign_id' => $campaign->id,
+                        'fund_id' => $campaign->fund->id,
+                    ])
+                    ->has(Meta::factory()->state(fn () => [
+                        'key' => fake()->randomElement(['woman_proposal', 'impact_proposal', 'ideafest_proposal']),
+                        'content' => fake()->randomElement([0, 1]),
+                        'model_type' => Proposal::class,
+                    ]))
+                    ->hasAttached($tags->random(), ['model_type' => Proposal::class])
+                    ->create();
+
+                // Attach each IdeaScale profile to at least one proposal
+                Concurrency::run([
+                    fn () => $IdeascaleProfiles->each(fn ($profile) => $proposals->random()->users()->attach($profile)),
+                    fn () => $groups->each(fn ($group) => $proposals->random()->groups()->attach($group)),
+                    fn () => $communities->each(fn ($community) => $proposals->random()->communities()->attach($community)),
+                    fn () => $proposals->each(function ($proposal) use ($IdeascaleProfiles, $groups, $communities) {
+                        $proposal->users()->attach($IdeascaleProfiles->random(fake()->randomElement([1, 2, 3, 4, 5])));
+                        $proposal->groups()->attach($groups->random());
+                        $proposal->communities()->attach($communities->random());
+                    }),
+                ]);
+            });
     }
 }
