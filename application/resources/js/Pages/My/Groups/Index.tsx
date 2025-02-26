ba57baca390e2a4b27a4d1103d3822af789a1831
Grace Mugoiri <gracemugoiri@gmail.com>
2025-02-26 21:40:57 +0300

    feat add FiltersProvider

--- application/resources/js/Pages/My/Groups/Index.tsx
+++ application/resources/js/Pages/My/Groups/Index.tsx
@@ -4,29 +4,38 @@ import { useTranslation } from 'react-i18next';
 import MyLayout from "@/Pages/My/MyLayout";
 import GroupData = App.DataTransferObjects.GroupData;
 import { PaginatedData } from '../../../../types/paginated-data';
-import GroupsList from './Partials/GroupsList';
+import MyGroupsList from './Partials/GroupsList';
 import Paginator from '@/Components/Paginator';
+import { FiltersProvider } from '@/Context/FiltersContext';
+import { SearchParams } from '../../../../types/search-params';
+
 
 interface MyGroupsProps extends Record<string, unknown> {
     groups?: PaginatedData<GroupData[]>;
+    filters: SearchParams;
+    
 }
 
-export default function MyGroups({groups}: MyGroupsProps) {
+export default function MyGroups({groups, filters}: MyGroupsProps) {
     const { t } = useTranslation();
+
     return (
-        <MyLayout >
+        <FiltersProvider defaultFilters={filters ?? {}}>
+            
+            <MyLayout>
             <Head title="My Groups" />
 
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
                 <div className="text-center text-content">
-                    <GroupsList groups={groups?.data ?? []} />
+                <MyGroupsList groups={groups?.data ?? []} />
                 </div>
             </div>
-                {/* {groups && (
-                    <Paginator
-                        pagination={groups}
-                    />
-                )} */}
-        </MyLayout>
+            {groups && (
+                <Paginator
+                pagination={groups}
+                />
+            )}
+            </MyLayout>
+        </FiltersProvider>
     );
 }
