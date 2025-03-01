42cf5381667e8b5edee8cb75c4a751dd50ee6ed1
Emmanuel Titi <emmanueltiti370@gmail.com>
2025-03-01 09:19:18 +0000

    Feature/ln 1613 ce implement nft endpoint for completed projects proxy to lido

---
+++ application/config/cardano-nft-maker-laravel.php
@@ -0,0 +1,8 @@
+<?php
+
+// config for Lidonation/CardanoNftMaker
+return [
+    'auth_key' => env('MAKER_AUTH_KEY', ''),
+    'baseUrl' => env('MAKER_BASE_URL', 'https://studio-api.preprod.nmkr.io'),
+    'project_uuid' => env('MAKER_PROJECT_UUID',)
+];
