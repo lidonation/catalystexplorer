33bb11c417411d622a62c14b9aa69930b0de1e14
WillardDev <willex.ox@gmail.com>
2025-03-03 20:55:35 +0300

    feat(nft and tx models): ln-1612 added nft and tx models with their nova resources

---
+++ application/app/Models/Nft.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\HasAuthor;
+use App\Models\Traits\HasMetaData;
+use App\Models\Traits\HasModel;
+use App\Models\Traits\HasTranslations;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Lidonation\CardanoNftMaker\Interfaces\CardanoNftInterface;
+use Lidonation\CardanoNftMaker\Traits\NftServiceTrait;
+
+class Nft extends Model implements CardanoNftInterface, HasLink
+{
+    use HasAuthor, HasMetaData, HasModel, HasTranslations, NftServiceTrait, SoftDeletes;
+
+    protected $hidden = ['artist_id', 'user_id', 'deleted_at', 'model_type', 'model_id'];
+
+    protected $guarded = [];
+
+    public array $translatable = [
+        'name',
+        'description',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'name' => 'array',
+            'metadata' => AsArrayObject::class,
+            'minted_at' => 'date:Y-m-d',
+            'created_at' => 'datetime:Y-m-d',
+            'updated_at' => 'datetime:Y-m-d',
+        ];
+    }
+
+    public function artist(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'artist_id');
+    }
+}
