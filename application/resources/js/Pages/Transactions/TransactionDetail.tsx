04ce99e94acbc7eeb983cb2255bb93bfaa56b5bc
Noel Wesley O. <noelwesley8750@gmail.com>
2025-03-25 09:33:42 +0000

    Feature/ln 1647 catalyst transaction list page

---
+++ application/resources/js/Pages/Transactions/TransactionDetail.tsx
@@ -0,0 +1,40 @@
+import Title from '@/Components/atoms/Title';
+import { Head } from '@inertiajs/react';
+import { useTranslation } from "react-i18next";
+import TransactionData = App.DataTransferObjects.TransactionData;
+
+interface TransactionDetailProps {
+  transaction: TransactionData;
+  metadataLabels: { id: number; name: string }[]; // Array of label objects
+  primaryLabel: string;
+}
+
+export default function TransactionDetail({
+  transaction,
+  metadataLabels,
+  primaryLabel
+}: TransactionDetailProps) {
+
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Head title="Transaction" />
+
+      <header>
+        <div className='container'>
+          <Title level='1'>Transaction</Title>
+        </div>
+        <div className='container'>
+          <p className="text-content">
+            View transaction details and related information
+          </p>
+        </div>
+      </header>
+
+      <div className="flex flex-col h-screen w-full items-center justify-center">
+        <Title level='2'>{t('comingSoon')}</Title>
+      </div>
+    </>
+  );
+}
