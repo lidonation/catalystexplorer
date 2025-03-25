e9ba47263d3b1e343d9b5d8aa5bfda84862d770b
vwambua <victorwambua004@gmail.com>
2025-03-25 17:47:34 +0300

    fix/communities-page-records-dont-exist

--- application/resources/js/Pages/Communities/Partials/CommunitiesList.tsx
+++ application/resources/js/Pages/Communities/Partials/CommunitiesList.tsx
@@ -1,3 +1,5 @@
+import RecordsNotFound from '@/Layouts/RecordsNotFound';
+import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
 import { PaginatedData } from '../../../../types/paginated-data';
 import CommunityCard from './CommunityCard';
@@ -8,6 +10,23 @@ interface CommunitesProps {
 }
 
 const CommunitiesList: React.FC<CommunitesProps> = ({ communities }) => {
+    if (!communities.data?.length) {
+        return (
+            <div className="w-full">
+                <AnimatePresence>
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.4, ease: 'easeIn' }}
+                    >
+                        <RecordsNotFound context="communities" searchTerm="" />
+                    </motion.div>
+                </AnimatePresence>
+            </div>
+        );
+    }
+
     return (
         <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
             {communities.data &&
