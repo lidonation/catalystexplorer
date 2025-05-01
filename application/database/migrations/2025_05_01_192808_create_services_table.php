e90c350fcdde74ce0436dc58dd5736c1c8540998
noelwesleey64 <noelwesley8750@gmail.com>
2025-05-02 00:50:06 +0300

    feat(service): ln-1748-ce fixing pipeline issues

--- application/database/migrations/2025_05_01_192808_create_services_table.php
+++ application/database/migrations/2025_05_01_192808_create_services_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->enum('type', ServiceTypeEnum::values());
+            $table->enum('type', ServiceTypeEnum::toValues()); 
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
