8828d7bfb71f1ff6e2e6e0dd914ecc380b848c02
Cindy <cindyjoan2000@gmail.com>
2025-05-28 12:31:37 +0300

    fix: allowing scrolling to view more items

--- application/resources/js/Components/layout/DesktopSidebar.tsx
+++ application/resources/js/Components/layout/DesktopSidebar.tsx
@@ -1,5 +1,5 @@
-import {usePage} from '@inertiajs/react';
-import {useTranslation} from 'react-i18next';
+import { usePage } from '@inertiajs/react';
+import { useTranslation } from 'react-i18next';
 import CatalystLogo from '../atoms/CatalystLogo';
 import AppNavigation from './AppNavigation';
 import ThemeSwitcher from './ThemeSwitcher';
@@ -8,50 +8,51 @@ import UserNavigation from './UserNavigation';
 import User = App.DataTransferObjects.UserData;
 
 function DesktopSidebar(props: any) {
-    const {t} = useTranslation();
-    const {auth} = usePage().props;
+    const { t } = useTranslation();
+    const { auth } = usePage().props;
     const { url } = usePage();
-    const {...rest} = props;
+    const { ...rest } = props;
     const isOnMyRoute = url.includes('/my/');
-    
+
     const currentPath = window.location.pathname;
-    
-    if (
-        currentPath.includes('login') ||
-        currentPath.includes('register')
-    ) {
+
+    if (currentPath.includes('login') || currentPath.includes('register')) {
         return null;
     }
-    
+
     return (
         <aside
             {...rest}
             className={`flex h-full w-full flex-col ${!isOnMyRoute ? 'justify-between' : ''} gap-8`}
             aria-label={t('navigation.desktop.sidebar')}
         >
-            <section className="flex h-6 shrink-0 items-center  sm:pt-8">
-                <CatalystLogo className="w-full"/>
+            <section className="flex h-6 shrink-0 items-center sm:pt-8">
+                <CatalystLogo className="w-full" />
             </section>
 
-            <section className={`flex flex-col gap-6 ${!isOnMyRoute ? 'overflow-y-auto ' : ''}`}>
-                <AppNavigation/>
-            </section>
+            <section className="overflow-y-auto">
+                <section
+                    className={`flex flex-col gap-6 pb-5 ${!isOnMyRoute ? 'overflow-y-auto' : ''}`}
+                >
+                    <AppNavigation />
+                </section>
 
-            <section className="flex flex-col gap-6">
-                <div className="px-4">
-                    <div className="border-gray-persist border-t pt-5">
-                        <UserNavigation/>
-                    </div>
-                </div>
-
-                <div className="border-gray-persist flex flex-col gap-5 border-t pt-5">
+                <section className="flex flex-col gap-6">
                     <div className="px-4">
-                        <UserDetails user={auth?.user as unknown as User}/>
+                        <div className="border-gray-persist border-t pt-5">
+                            <UserNavigation />
+                        </div>
                     </div>
-                    <div className="bg-background-darker px-4 py-4">
-                        <ThemeSwitcher/>
+
+                    <div className="border-gray-persist flex flex-col gap-5 border-t pt-5">
+                        <div className="px-4">
+                            <UserDetails user={auth?.user as unknown as User} />
+                        </div>
+                        <div className="bg-background-darker px-4 py-4">
+                            <ThemeSwitcher />
+                        </div>
                     </div>
-                </div>
+                </section>
             </section>
         </aside>
     );
