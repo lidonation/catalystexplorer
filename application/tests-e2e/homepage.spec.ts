5831b435a7c69e5c8f6da83372bad38bf0516862
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2025-07-12 06:59:49 +0000

    Feature/ln 1806 ce testing framework

---
+++ application/tests-e2e/homepage.spec.ts
@@ -0,0 +1,17 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('Home Page', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+        await page.waitForLoadState('domcontentloaded');
+    });
+
+    test('has title', async ({ page }) => {
+        await expect(page).toHaveTitle('Catalyst Explorer - CatalystExplorer');
+    });
+
+    test('has global search', async ({ page }) => {
+        const searchForm = page.getByTestId('global-search-form');
+        await expect(searchForm).toBeVisible();
+    });
+});
