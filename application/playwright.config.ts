85e6696b3c47567e50c9d48f00ca4d044dc6ba6f
mbrian <chiebyrah@gmail.com>
2025-08-12 19:54:18 +0300

    update sgards and gitlab.CI

--- application/playwright.config.ts
+++ application/playwright.config.ts
@@ -6,11 +6,11 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     timeout: 120_000,
     retries: process.env.CI ? 2 : 0,
-    workers: 3,
+    workers: process.env.CI ? 1 : 3,
     reporter: [['html', { open: 'never' }], ['allure-playwright']],
 
     use: {
-        baseURL: 'https://preview.catalystexplorer.com/en',
+        baseURL: process.env.CI ? 'http://localhost:3000' : 'https://preview.catalystexplorer.com/en',
         trace: 'retain-on-failure',
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
