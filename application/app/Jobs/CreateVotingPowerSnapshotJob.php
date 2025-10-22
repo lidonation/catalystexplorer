a16b8573357241017d7213d7e854d0cf3134db84
noelwesleey64 <noelwesley8750@gmail.com>
2025-10-22 21:28:54 +0300

    feat(charts):converting voting power to lovelace when inmporting

--- application/app/Jobs/CreateVotingPowerSnapshotJob.php
+++ application/app/Jobs/CreateVotingPowerSnapshotJob.php
@@ -46,7 +46,7 @@ class CreateVotingPowerSnapshotJob implements ShouldQueue
 
             VotingPower::create([
                 'voter_id' => $stakeAddress,
-                'voting_power' => $votingPower,
+                'voting_power' => (int) round((float) $votingPower * 1_000_000),
                 'snapshot_id' => $this->snapshotId,
             ]);
         }
