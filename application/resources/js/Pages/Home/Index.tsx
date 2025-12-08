0fd7a7e6b3833df69aae76bb7cd0c1afbd895fbf
Darlington Kofa <darlington@lidonation.com>
2025-12-08 15:32:24 +0300

    chore: cleanup

--- application/resources/js/Pages/Home/Index.tsx
+++ application/resources/js/Pages/Home/Index.tsx
@@ -60,26 +60,26 @@ export default function Index({
             <div className="relative flex w-full flex-col justify-center gap-8">
                 <CatalystIntro />
 
-                {/*{((*/}
-                {/*    // New flat array structure*/}
-                {/*    Array.isArray(quickPitches) && quickPitches.length > 0*/}
-                {/*) || (*/}
-                {/*    // Legacy nested structure (ensure it's an object with 'regular')*/}
-                {/*    !Array.isArray(quickPitches) && 'regular' in (quickPitches || {}) && Array.isArray((quickPitches as { regular?: ProposalData[] }).regular) && (quickPitches as { regular?: ProposalData[] }).regular!.length > 0*/}
-                {/*)) && */}
-                {/*<section*/}
-                {/*    className="quickpitches-wrapper"*/}
-                {/*    data-testid="quickpitches-section"*/}
-                {/*>*/}
-                {/*    <div className="container">*/}
-                {/*        <WhenVisible*/}
-                {/*            fallback={<QuickPitchListLoading />}*/}
-                {/*            data="quickPitches"*/}
-                {/*        >*/}
-                {/*            <QuickPitchList quickPitches={quickPitches} activeFundId={quickPitchesFundId} />*/}
-                {/*        </WhenVisible>*/}
-                {/*    </div>*/}
-                {/*</section>}*/}
+                {((
+                    // New flat array structure
+                    Array.isArray(quickPitches) && quickPitches.length > 0
+                ) || (
+                    // Legacy nested structure (ensure it's an object with 'regular')
+                    !Array.isArray(quickPitches) && 'regular' in (quickPitches || {}) && Array.isArray((quickPitches as { regular?: ProposalData[] }).regular) && (quickPitches as { regular?: ProposalData[] }).regular!.length > 0
+                )) &&
+                <section
+                    className="quickpitches-wrapper"
+                    data-testid="quickpitches-section"
+                >
+                    <div className="container">
+                        <WhenVisible
+                            fallback={<QuickPitchListLoading />}
+                            data="quickPitches"
+                        >
+                            <QuickPitchList quickPitches={quickPitches} activeFundId={quickPitchesFundId} />
+                        </WhenVisible>
+                    </div>
+                </section>}
 
                 <section
                     className="annnouncements-wrapper"
